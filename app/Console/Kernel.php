f3577022d3e0c2a63cc056a67dd0429eb20c0914
liudongdong <dde-mail@qq.com>
2019-09-25 18:47:04 +0800

    Command Disco

---
+++ app/Console/Kernel.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Console;
+
+use Discuz\Console\Kernel as ConsoleKernel;
+
+class Kernel extends ConsoleKernel
+{
+    /**
+     * The Artisan commands provided by your application.
+     *
+     * @var array
+     */
+    protected $commands = [
+        //
+    ];
+
+    /**
+     * Register the commands for the application.
+     *
+     * @return void
+     */
+    protected function commands()
+    {
+        $this->load(__DIR__.'/Commands');
+    }
+}
