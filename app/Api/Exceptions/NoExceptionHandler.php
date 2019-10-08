c14edf596f2e3dbccb444f08f18bd5dcb562772a
leiyu <leiyu@comsenz-service.com>
2019-10-08 17:22:07 +0800

    api 异常处理

---
+++ app/Api/Exceptions/NoExceptionHandler.php
@@ -0,0 +1,47 @@
+<?php
+
+
+namespace App\Api\Exceptions;
+
+
+use Discuz\Http\Exception\RouteNotFoundException;
+use Exception;
+use Tobscure\JsonApi\Exception\Handler\ExceptionHandlerInterface;
+use Tobscure\JsonApi\Exception\Handler\ResponseBag;
+
+class NoExceptionHandler implements ExceptionHandlerInterface
+{
+
+    /**
+     * If the exception handler is able to format a response for the provided exception,
+     * then the implementation should return true.
+     *
+     * @param \Exception $e
+     *
+     * @return bool
+     */
+    public function manages(Exception $e)
+    {
+        // TODO: Implement manages() method.
+        return $e instanceof RouteNotFoundException;;
+    }
+
+    /**
+     * Handle the provided exception.
+     *
+     * @param \Exception $e
+     *
+     * @return \Tobscure\JsonApi\Exception\Handler\ResponseBag
+     */
+    public function handle(Exception $e)
+    {
+        // TODO: Implement handle() method.
+        $status = 404;
+        $error = [
+            'status' => (string) $status,
+            'code' => 'route_not_found'
+        ];
+
+        return new ResponseBag($status, [$error]);
+    }
+}
