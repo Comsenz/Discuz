c060769200c4bac14e0810d386c48c98755a687c
leiyu <leiyu@comsenz-service.com>
2019-10-10 14:57:23 +0800

    index page

--- app/Http/Controller/IndexController.php
+++ app/Http/Controller/IndexController.php
@@ -2,16 +2,23 @@
 
 namespace App\Http\Controller;
 
-use App\Models\User;
+use Discuz\Foundation\Application;
 use Discuz\Web\Controller\AbstractWebController;
+use Illuminate\Contracts\Filesystem\Factory as FileFactory;
 use Illuminate\View\Factory;
-use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
-use Psr\Http\Server\RequestHandlerInterface;
-use Zend\Diactoros\Response\HtmlResponse;
 
 class IndexController extends AbstractWebController
 {
+    protected $file;
+    protected $validator;
+
+    public function __construct(Application $app, Factory $view, FileFactory $file, \App\Validators\TestValidator $validator)
+    {
+        parent::__construct($app, $view);
+        $this->file = $file;
+        $this->validator = $validator;
+    }
 
     /**
      * @param ServerRequestInterface $request
@@ -20,7 +27,6 @@ class IndexController extends AbstractWebController
      */
     public function render(ServerRequestInterface $request, Factory $view)
     {
-
         $view = $view->make('app');
         $view->with('title', 'discuss');
 
