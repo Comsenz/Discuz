eb77a06b3aac45f444ff57fa7fa94c9df1cb8db9
leiyu <leiyu@comsenz-service.com>
2019-10-10 15:00:19 +0800

    index page

--- app/Http/Controller/IndexController.php
+++ app/Http/Controller/IndexController.php
@@ -2,24 +2,12 @@
 
 namespace App\Http\Controller;
 
-use Discuz\Foundation\Application;
 use Discuz\Web\Controller\AbstractWebController;
-use Illuminate\Contracts\Filesystem\Factory as FileFactory;
 use Illuminate\View\Factory;
 use Psr\Http\Message\ServerRequestInterface;
 
 class IndexController extends AbstractWebController
 {
-    protected $file;
-    protected $validator;
-
-    public function __construct(Application $app, Factory $view, FileFactory $file, \App\Validators\TestValidator $validator)
-    {
-        parent::__construct($app, $view);
-        $this->file = $file;
-        $this->validator = $validator;
-    }
-
     /**
      * @param ServerRequestInterface $request
      * @param Factory $view
