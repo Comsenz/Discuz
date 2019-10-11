ec818c9de51855c03936cd4da24394110563061a
Xu Bin <xubin727@163.com>
2019-10-11 13:50:28 +0800

    kkk

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
-
 use App\Api\Controller as ApiController;
 
 $route->get('/users', 'users.list', ApiController\ListUsersController::class);
+$route->get('/pay/order', 'pay.order', ApiController\PayOrderController::class);
+$route->get('/pay/qrcode', 'pay.qrcode', ApiController\PayQrcodeController::class);
+$route->get('/pay/qr/img', 'pay.qr.img', ApiController\PayQrImgController::class);
+$route->get('/pay/test', 'pay.test', ApiController\PayTestController::class);
