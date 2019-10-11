0253cf44b2c158a915c96693b2696fcd5a64816c
xubin727 <xubin727@163.com>
2019-10-11 16:43:07 +0800

    supply wxpayapi package

---
+++ xubin/wxpayapi/WxPayData/WxPayNotifyResults.php
@@ -0,0 +1,42 @@
+<?php
+namespace xubin\wxpayapi\WxPayData;
+
+use xubin\wxpayapi\WxPayConfigInterface;
+use xubin\wxpayapi\WxPayException;
+
+
+
+/**
+ * 2015-06-29 修复签名问题
+ **/
+// require_once "WxPay.Config.Interface.php";
+// require_once "WxPay.Exception.php";
+
+
+
+
+/**
+ *
+ * 回调回包数据基类
+ *
+ **/
+class WxPayNotifyResults extends WxPayResults
+{
+	/**
+	 * 将xml转为array
+	 *
+	 * @param WxPayConfigInterface $config
+	 * @param string $xml
+	 *
+	 * @return WxPayNotifyResults
+	 * @throws WxPayException
+	 */
+	public static function Init($config, $xml)
+	{
+		$obj = new self();
+		$obj->FromXml($xml);
+		//失败则直接返回失败
+		$obj->CheckSign($config);
+		return $obj;
+	}
+}
