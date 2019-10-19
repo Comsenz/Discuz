bd6b944a1270fe188133451156a609d91f354099
gudanxin1314 <56377852+gudanxin1314@users.noreply.github.com>
2019-10-19 19:51:31 +0800

    提交字体图标库
    提交字体图标库

--- resources/frame/src/template/default/view/m_site/home/indexView.vue
+++ resources/frame/src/template/default/view/m_site/home/indexView.vue
@@ -3,11 +3,18 @@
 <template>
     <div>
       <p>移动端首页</p>
+      <span class="icon iconfont" style="color:#336699;">&#xe60a;</span>
     </div>
 </template>
 
 <script>
+import  '../../../../../../static/css/iconfont.css';
+
+
 export default {
     name: "index-view"
 }
+
+
+
 </script>
