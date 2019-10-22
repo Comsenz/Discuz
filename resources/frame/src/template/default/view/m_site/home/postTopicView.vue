00d77687f13e967b3e3dd312b7b4d8fb2abd0ea7
YangPengPc <1324901147@qq.com>
2019-10-22 16:42:43 +0800

    前台移动端登录界面-发布主题文件提交

---
+++ resources/frame/src/template/default/view/m_site/home/postTopicView.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="post-topic-box">
+      <header class="post-topic-header">
+        <span @click="backClick"  class="icon iconfont post-topic-header-icon" >&#xe605;</span>
+        <h2>{{headerTitle}}</h2>
+        <span  class="icon iconfont post-topic-header-icon" >&#xe60c;</span>
+      </header>
+      <div class="post-topic-form">
+        <textarea id="post-topic-form-text" name="post-topic"   placeholder="请输入内容"></textarea>
+      </div>
+      <footer class="post-topic-footer">
+        <div class="post-topic-footer-left">
+          <span  class="icon iconfont post-topic-header-icon" >&#xe608;</span>
+          <span  class="icon iconfont post-topic-header-icon" >&#xe612;</span>
+          <span  class="icon iconfont post-topic-header-icon" >&#xe60a;</span>
+        </div>
+        <div class="post-topic-footer-right">
+          <span>{{selectSort}}</span>
+          <span @click="dClick"  class="icon iconfont post-topic-header-icon" >&#xe60a;</span>
+        </div>
+      </footer>
+    </div>
+</template>
+
+<script>
+import '../../../../../../static/css/iconfont.css';
+import '../../../scss/mobile/postTopicView.scss'
+import postTopicCon from '../../../controllers/m_site/home/postTopic/postTopicCon'
+export default {
+    name: "post-topic",
+  ...postTopicCon,
+}
+</script>
