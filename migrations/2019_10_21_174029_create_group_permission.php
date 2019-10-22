e32e32cf05a726f86f9b9e444a8565d8901e6313
yulei745 <yleimm@gmail.com>
2019-10-22 14:12:32 +0800

    migration command migration tables

---
+++ migrations/2019_10_21_174029_create_group_permission.php
@@ -0,0 +1,31 @@
+<?php
+
+use Discuz\Database\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateGroupPermission extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $this->schema()->create('group_permission', function (Blueprint $table) {
+            $table->unsignedInteger('group_id')->comment('用户组ID');
+            $table->string('permission')->comment('权限名称');
+            $table->primary(['group_id', 'permission']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $this->schema()->dropIfExists('group_permission');
+    }
+}
