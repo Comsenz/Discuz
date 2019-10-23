642b9e7258bf327cf15d680e62b57f84fad866c1
YangPengPc <1324901147@qq.com>
2019-10-23 11:19:30 +0800

    前台移动端登录界面-支付费用文件

---
+++ resources/frame/src/template/default/view/m_site/login/payTheFeeView.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="pay-the-fee-box">
+      <PayHeader></PayHeader>
+      <main class="pay-the-fee-main">
+        <div class="pay-the-fee-titel">
+          <h2>支付费用</h2>
+        </div>
+        <button class="pay-the-fee-btn weui-btn weui-btn_primary">立即付费,获得权限</button>
+        <p class="pay-the-fee-title-footer">￥20.00 / 永久有效</p>
+      </main>
+      <div class="pay-the-fee-permission">
+        <div class="pay-the-fee-per-title">
+          <h3>作为成员，您将获得以下权限</h3>
+        </div>
+        <div class="pay-the-fee-per-list">
+          <h3 class="pay-the-fee-per-name">帖子操作</h3>
+          <ul>查看主题</ul>
+          <ul>发图文贴</ul>
+        </div>
+        <div class="pay-the-fee-per-list-footer">
+          <p @click="leapFrogClick">跳过，进入首页</p>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+import '../../../scss/mobile/login/loginSignUpModule.scss';
+import payTheFeeCon from '../../../controllers/m_site/login/payTheFeeCon';
+export default {
+    name: "pay-the-fee-view",
+  ...payTheFeeCon
+}
+</script>
+
