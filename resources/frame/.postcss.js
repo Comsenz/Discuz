5bf5e0fa60f33e83d78ccd4820bd05f3edc2544a
YangPengPc <1324901147@qq.com>
2019-10-25 18:28:38 +0800

    前台移动端-增加postcss插件

---
+++ resources/frame/.postcss.js
@@ -0,0 +1,17 @@
+module.exports = {
+  "plugins": {
+    "postcss-import": {},
+    "postcss-url": {},
+    // to edit target browsers: use "browserslist" field in package.json
+    "autoprefixer": {},
+    "postcss-pxtorem": { // 此处为添加部分
+      rootValue: 32, // 对应16px 适配移动端750px宽度
+      unitPrecision: 5,
+      propList: ['*'],
+      selectorBlackList: [],
+      replace: true,
+      mediaQuery: false,
+      minPixelValue: 0
+    }
+  }
+}
