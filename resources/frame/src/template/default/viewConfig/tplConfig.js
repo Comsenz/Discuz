7fdf8ca15750791c4e1375e25ab01ff6282e0d53
YangPengPc <1324901147@qq.com>
2019-10-28 10:01:10 +0800

    前台移动端-我的-修改资料-添加头像路径 前台移动端-路由-整理‘我的’名称

--- resources/frame/src/template/default/viewConfig/tplConfig.js
+++ resources/frame/src/template/default/viewConfig/tplConfig.js
@@ -191,7 +191,7 @@ export default {
           require(['../view/m_site/myInfo/modifyPhoneView'],resolve)
         },
         metaInfo:{
-          title:"回复主题"
+          title:"修改手机号"
         }
       },
       'change-pwd':{
@@ -199,7 +199,7 @@ export default {
           require(['../view/m_site/myInfo/changePasswordView'],resolve)
         },
         metaInfo:{
-          title:"回复主题"
+          title:"修改密码"
         }
       },
       'withdraw':{
@@ -207,7 +207,7 @@ export default {
           require(['../view/m_site/myInfo/withdrawView'],resolve)
         },
         metaInfo:{
-          title:"回复主题"
+          title:"提款"
         }
       },
 
