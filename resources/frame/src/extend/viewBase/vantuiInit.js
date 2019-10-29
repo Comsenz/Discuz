951284c25206fe35638169d13edf9b6447a5423e
YangPengPc <1324901147@qq.com>
2019-10-29 16:51:12 +0800

    前台移动端-cell\cell-group

--- resources/frame/src/extend/viewBase/vantuiInit.js
+++ resources/frame/src/extend/viewBase/vantuiInit.js
@@ -4,3 +4,5 @@ import { Button,Popup,Cell,Tab,Tabs,Field,Divider,CellGroup } from 'vant';
 
 Vue.use(Button).use(Popup).use(Cell).use(Tab).use(Tabs).use(Field).use(Divider).use(CellGroup);
 import '../../template/default/less/m_site/vant/tabs/index.less';
+import '../../template/default/less/m_site/vant/cell-group/index.less';
+import '../../template/default/less/m_site/vant/cell/index.less';
--- resources/frame/src/template/default/view/m_site/login/loginUserView.vue
+++ resources/frame/src/template/default/view/m_site/login/loginUserView.vue
@@ -6,22 +6,39 @@
           <p class="login-user-title-p">用户名登录</p>
         </div>
 
-        <form class="weui-cells__group weui-cells__group_form user-login-form">
-          <div class="weui-cells weui-cells_form login-user-input-form">
-            <div class="weui-cell">
-              <div class="weui-cell__hd"><label class="weui-label">用户名</label></div>
-              <div class="weui-cell__bd">
-                <input id="js_input" class="weui-input" placeholder="请输入您的用户名">
-              </div>
-            </div>
-            <div class="weui-cell">
-              <div class="weui-cell__hd"><label class="weui-label">密码</label></div>
-              <div class="weui-cell__bd">
-                <input id="js_input" class="weui-input" type="password" placeholder="请填写密码">
-              </div>
+        <!--<form class="weui-cells__group weui-cells__group_form user-login-form">
+        <div class="weui-cells weui-cells_form login-user-input-form">
+          <div class="weui-cell">
+            <div class="weui-cell__hd"><label class="weui-label">用户名</label></div>
+            <div class="weui-cell__bd">
+              <input id="js_input" class="weui-input" placeholder="请输入您的用户名">
             </div>
           </div>
+          <div class="weui-cell">
+            <div class="weui-cell__hd"><label class="weui-label">密码</label></div>
+            <div class="weui-cell__bd">
+              <input id="js_input" class="weui-input" type="password" placeholder="请填写密码">
+            </div>
+          </div>
+        </div>
+      </form>-->
+
+        <form class="user-login-form">
+          <van-cell-group>
+            <van-field
+              clearable
+              label="用户名"
+              placeholder="请输入用户名"
+            />
+
+            <van-field
+              type="password"
+              label="密码"
+              placeholder="请输入密码"
+            />
+          </van-cell-group>
         </form>
+
 
         <div class="login-user-btn">
           <button class="weui-btn weui-btn_primary login-btn">登录</button>
