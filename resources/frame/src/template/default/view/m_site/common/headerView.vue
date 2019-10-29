c7d3f3bccf93c8f57a38ccff4b4021fbe0d8a274
gudanxin1314 <56377852+gudanxin1314@users.noreply.github.com>
2019-10-29 15:41:12 +0800

    vant修改

--- resources/frame/src/template/default/view/m_site/common/headerView.vue
+++ resources/frame/src/template/default/view/m_site/common/headerView.vue
@@ -86,11 +86,11 @@
     </div>
 
     <div class="navBox" id="testNavBar" :class="{'fixedNavBar': isfixNav}" v-if="navShow">
-      <!-- <van-tabs v-model="navActi">
+      <van-tabs v-model="navActi" sticky="false">
         <van-tab v-for="index in 8" :title="'标签 ' + index" :key="index">
           内容 {{ index }}
         </van-tab>
-      </van-tabs> -->
+      </van-tabs>
       <div class="navBarBox">
         <ul class="navBarCon">
           <li v-for="(todo, index) in todos" v-on:click="addClass(index,$event)" v-bind:class="{ navActi:index==current}">{{ todo.text }}</li>
@@ -106,145 +106,9 @@
 <script>
 import mSiteHeader from '../../../controllers/m_site/common/header';
 import '../../../scss/m_site/mobileIndex.scss';
-import '../../../scss/vantLess.less';
 export default {
   name: "headerView",
   ...mSiteHeader
 }
-// export default {
-  // data: function() {
-  //   return {
-  //     isfixNav: false,
-  //     isfixHead: false,
-  //     isShow: false,
-  //     isHeadShow: false,
-  //     showHeader: false,
-  //     showSidebar: false,
-  //     showMask: false,
-  //     sidebarList1: [
-  //       {
-  //         name: '我的资料',
-  //         path: 'login', // 跳转路径
-  //         query: { // 跳转参数
-  //         index: 1
-  //         },
-  //           enentType: ''
-  //       },
-  //       {
-  //         name: '我的钱包',
-  //         path: 'wallent', // 跳转路径
-  //         query: { // 跳转参数
-  //         index: 2
-  //         },
-  //           enentType: ''
-  //       },
-  //       {
-  //         name: '我的收藏',
-  //         path: 'collection', // 跳转路径
-  //         query: { // 跳转参数
-  //         index: 3
-  //         },
-  //           enentType: ''
-  //       }
-  //     ],
-  //     sidebarList2: [
-  //       {
-  //         name: '圈子信息',
-  //         path: 'login', // 跳转路径
-  //         query: { // 跳转参数
-  //         index: 1
-  //         },
-  //           enentType: ''
-  //       },
-  //       {
-  //         name: '圈子管理',
-  //         path: 'login', // 跳转路径
-  //         query: { // 跳转参数
-  //           index: 2
-  //         },
-  //         enentType: ''
-  //       },
-  //       {
-  //         name: '退出登录',
-  //         path: '', // 跳转路径
-  //         query: { // 跳转参数
-  //           index: 3
-  //         },
-  //         enentType: 1 // 事件类型
-  //       }
-  //     ],
-  //     sidebarList3: [
-  //       {
-  //         name: '邀请朋友',
-  //         path: 'login', // 跳转路径
-  //         query: { // 跳转参数
-  //         index: 1
-  //         },
-  //           enentType: ''
-  //       }
 
-  //     ]
-
-  //   }
-  // },
-  // methods: {
-
-    // // 先分别获得id为testNavBar的元素距离顶部的距离和页面滚动的距离
-    // // 比较他们的大小来确定是否添加fixedHead样式
-    // handleTabFix() {
-    //     var scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop
-    //     var offsetTop = document.querySelector('#testNavBar').offsetTop;
-    //     if(scrollTop > offsetTop){
-    //       // console.log('大于');
-    //       this.showHeader = true;
-    //       this.isfixHead = true;
-    //       this.isfixNav = true;
-    //       // scrollTop > offsetTop ? this.isfixHead = true : this.isfixHead = false;
-    //       // scrollTop < offsetTop ? this.isfixNav = true : this.isfixNav = false
-    //     } else {
-    //       // console.log('小于');
-    //       this.showHeader = false;
-    //       this.isfixHead = false;
-    //       this.isfixNav = false;
-    //       // scrollTop > offsetTop ? this.isfixHead = false : this.isfixHead = true;
-    //       // scrollTop < offsetTop ? this.isfixNav = false : this.isfixNav = true
-    //     };
-
-    //   },
-    //   searchJump () {
-
-    //   },
-    //   backUrl () {
-    //     // 返回上一级
-    //     window.history.go(-1)
-    //   },
-    //   bindSidebar () {
-    //     // 是否显示侧边栏
-    //     this.showSidebar = !this.showSidebar;
-    //     this.showMask =  !this.showMask;
-    //   },
-    //   hideSidebar(){
-    //     this.showSidebar = false;
-    //     this.showMask =  false;
-    //   },
-    //   bindEvent (typeName) {
-    //     if (typeName == 1) {
-    //       this.LogOut()
-    //     }
-    //   },
-    //   LogOut () {
-    //     console.log('测试');
-    //   }
-
-  // },
-
-  // mounted: function() {
-  //   // this.getVote();
-  //   window.addEventListener('scroll', this.handleTabFix, true);
-  // },
-  // beforeRouteLeave (to, from, next) {
-  //    window.removeEventListener('scroll', this.handleTabFix, true)
-  //    next()
-  // }
-// }
-// </script>
+</script>
