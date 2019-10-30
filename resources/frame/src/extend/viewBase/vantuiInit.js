5ea9a21eb6a66ddfba973cbed2f41cb3dd02d2ad
YangPengPc <1324901147@qq.com>
2019-10-30 14:49:13 +0800

    前台移动端-更改样式文件名

--- resources/frame/src/extend/viewBase/vantuiInit.js
+++ resources/frame/src/extend/viewBase/vantuiInit.js
@@ -3,7 +3,7 @@ import { Button,Popup,Cell,Tab,Tabs,Field,Divider,CellGroup,Search } from 'vant'
 
 
 Vue.use(Button).use(Popup).use(Cell).use(Tab).use(Tabs).use(Field).use(Divider).use(CellGroup).use(Search);
-import '../../template/default/less/m_site/vant/tabs/index.less';
-import '../../template/default/less/m_site/vant/cell-group/index.less';
-import '../../template/default/less/m_site/vant/cell/index.less';
-import '../../template/default/less/m_site/vant/search/index.less';
+import '../../template/default/less/m_site/modules/tabs/index.less';
+import '../../template/default/less/m_site/modules/cell-group/index.less';
+import '../../template/default/less/m_site/modules/cell/index.less';
+import '../../template/default/less/m_site/modules/search/index.less';
