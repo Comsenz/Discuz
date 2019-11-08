0007eb57ef6f2f14c045e9271d71faf70b2057ac
YangPengPc <1324901147@qq.com>
2019-11-08 18:41:37 +0800

    后台-Index页面

--- resources/frame/src/admin/view/site/home/homeView.vue
+++ resources/frame/src/admin/view/site/home/homeView.vue
@@ -1,11 +1,14 @@
 <template>
-    <div>
+    <div class="home-box">
       首页
     </div>
 </template>
 
 <script>
+import '../../../scss/pageStyle.scss';
+import homeCon from '../../../controllers/home/homeCon';
 export default {
-    name: "home-view"
+    name: "home-view",
+  ...homeCon
 }
 </script>
