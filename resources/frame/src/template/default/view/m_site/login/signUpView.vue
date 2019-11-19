79ad3e14ee5d2512d46fab3d243693f7b1ae5346
gudanxin1314 <56377852+gudanxin1314@users.noreply.github.com>
2019-11-19 16:21:08 +0800

    细节修复

--- resources/frame/src/template/default/view/m_site/login/signUpView.vue
+++ resources/frame/src/template/default/view/m_site/login/signUpView.vue
@@ -62,11 +62,12 @@
     </div>
 </template>
 <style>
-  .toast {            position: fixed;            z-index: 2000;            left: 50%;            top:45%;            transition:all .5s;            -webkit-transform: translateX(-50%) translateY(-50%);               -moz-transform: translateX(-50%) translateY(-50%);                -ms-transform: translateX(-50%) translateY(-50%);                 -o-transform: translateX(-50%) translateY(-50%);                    transform: translateX(-50%) translateY(-50%);            text-align: center;            border-radius: 5px;            color:#FFF;            background: rgba(17, 17, 17, 0.7);            height: 45px;            line-height: 45px;            padding: 0 15px;            max-width: 150px;        }
+  /* .toast { position: fixed; z-index: 200; left: 50%; top:45%; transition:all .5s; -webkit-transform: translateX(-50%) translateY(-50%); -moz-transform: translateX(-50%) translateY(-50%); -ms-transform: translateX(-50%) translateY(-50%); -o-transform: translateX(-50%) translateY(-50%); transform: translateX(-50%) translateY(-50%); text-align: center; border-radius: 5px; color:#FFF; background: rgba(17, 17, 17, 0.7);      height: 45px; line-height: 45px; padding: 0 15px; max-width: 150px; } */
 </style>
 <script>
 import '../../../scss/m_site/login/loginSignUpModule.scss';
 import signUpCon from '../../../controllers/m_site/login/signUpCon';
+import  '../../../scss/m_site/mobileIndex.scss';
 export default {
     name: "sign-up-view",
   ...signUpCon
