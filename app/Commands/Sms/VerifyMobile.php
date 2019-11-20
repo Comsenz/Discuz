c5c94bc1874b88f7dcf6281eb0ec66d768c79cab
yulei745 <yleimm@gmail.com>
2019-11-20 10:58:00 +0800

    mobile_confirmed

--- app/Commands/Sms/VerifyMobile.php
+++ app/Commands/Sms/VerifyMobile.php
@@ -61,6 +61,7 @@ class VerifyMobile
         $this->controller->serializer = UserSerializer::class;
         if($this->actor->exists) {
             $this->actor->mobile = $this->mobileCode->mobile;
+            $this->actor->mobile_confirmed = 1;
             $this->actor->save();
             $this->mobileCode->user = $this->actor;
         }
