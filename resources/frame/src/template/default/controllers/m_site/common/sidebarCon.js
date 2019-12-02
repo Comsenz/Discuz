c51b8d54691ac21a3f2ef90ab4bb236439760e95
gudanxin1314 <56377852+gudanxin1314@users.noreply.github.com>
2019-12-02 11:12:54 +0800

    侧边栏bug修复

--- resources/frame/src/template/default/controllers/m_site/common/sidebarCon.js
+++ resources/frame/src/template/default/controllers/m_site/common/sidebarCon.js
@@ -9,61 +9,85 @@ export default {
         avatarUrl:'',
         username:'',
         mobile:'',
-        userId:'',
+        // userId:'',
 		    sidebarList1: [
 	        {
+            text:'我的资料',
 	          name: '我的资料',
 	          path: 'login', // 跳转路径
 	          // query: { // 跳转参数
            //    index: 1
 	          // },
-            query:'',
+            // query:{
+            //   userId:''
+            // },
 	          enentType: ''
 	        },
 	        {
-	          name: '我的钱包',
-	          path: 'my-wallet', // 跳转路径
-	          query:'',
+            text:'我的钱包',
+	          name: 'my-wallet',
+	          path: '/my-wallet', // 跳转路径
+	          // query:{
+	          //   userId:''
+	          // },
 	          enentType: ''
 	        },
 	        {
-	          name: '我的收藏',
-	          path: 'my-collection', // 跳转路径
-	          query:'',
+            text:'我的收藏',
+	          name: 'my-collection',
+	          path: '/my-collection', // 跳转路径
+	          // query:{
+	          //   userId:''
+	          // },
 	          enentType: ''
 	        },
 	        {
-	          name: '我的通知',
-	          path: 'my-notice', // 跳转路径
-	          query:'',
+            text:'我的通知',
+	          name: 'my-notice',
+	          path: '/my-notice', // 跳转路径
+	          // query:{
+	          //   userId:''
+	          // },
 	          enentType: ''
 	        }
 	      ],
 	      sidebarList2: [
 	        {
-	          name: '圈子信息',
-	          path: 'circle-info', // 跳转路径
-	          query:'',
+            text:'圈子信息',
+	          name: 'circle-info',
+	          path: '/circle-info', // 跳转路径
+	          // query:{
+	          //   userId:''
+	          // },
 	          enentType: ''
 	        },
 	        {
-	          name: '圈子管理',
-	          path: 'management-circles', // 跳转路径
-	          query:'',
+            text:'圈子管理',
+	          name: 'management-circles',
+	          path: '/management-circles', // 跳转路径
+	          // query:{
+	          //   userId:''
+	          // },
 	          enentType: ''
 	        },
 	        {
-	          name: '退出登录',
-	          path: 'login-user', // 跳转路径
-	          query:'',
+            text:'退出登录',
+	          name: 'login-user',
+	          path: '/login-user', // 跳转路径
+	          // query:{
+	          //   userId:''
+	          // },
 	          enentType: 1 // 事件类型
 	        }
 	      ],
 	      sidebarList3: [
 	        {
-	          name: '邀请朋友',
-	          path: 'invite-join', // 跳转路径
-	          query:'',
+            text:'邀请朋友',
+	          name: 'invite-join',
+	          path: '/invite-join', // 跳转路径
+	          // query:{
+	          //   userId:''
+	          // },
 	          enentType: ''
 	        }
 
@@ -72,23 +96,7 @@ export default {
   },
   created: function() {
     this.getUserInfo();
-    var userId = browserDb.getLItem('tokenId');
-    for(var i=0;i<this.sidebarList1.length;i++){
-
-      this.sidebarList1[i].query = userId;
-      console.log(this.sidebarList1[i].query);
-
-    };
-    for(var j=0;j<this.sidebarList1.length;j++){
-      this.sidebarList2[j].query = userId;
-      console.log(this.sidebarList2[j].query);
-
-    };
-    for(var h=0;h<this.sidebarList3.length;h++){
-      this.sidebarList3[h].query = userId;
-    };
   },
-
   methods:{
   //获取用户信息
   getUserInfo(){
@@ -100,11 +108,12 @@ export default {
         this.mobile = data.data.attributes.mobile;
       });
   },
-  sidebarUrl(url,query,enentType){
+  sidebarUrl(url,enentType){
+    var userId = browserDb.getLItem('tokenId');
     if(enentType == 1){
       this.$router.push({ path:url});
     } else {
-      this.$router.push({ path:url+'/'+query});
+      this.$router.push({ path:url+'/'+userId});
     }
   }
 
