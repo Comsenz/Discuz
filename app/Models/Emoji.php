c9005f21346489e9fce6a1d54a1d0a5593ed4997
ldd <dde-mail@qq.com>
2019-12-06 14:21:37 +0800

    [add] emoji render

--- app/Models/Emoji.php
+++ app/Models/Emoji.php
@@ -1,46 +1,9 @@
 <?php
-declare(strict_types=1);
 
 namespace App\Models;
 
-use Discuz\Database\ScopeVisibilityTrait;
-use Discuz\Foundation\EventGeneratorTrait;
 use Illuminate\Database\Eloquent\Model;
 
 class Emoji extends Model
 {
-    use EventGeneratorTrait;
-    use ScopeVisibilityTrait;
-
-    /**
-     * 与模型关联的数据表.
-     *
-     * @var string
-     */
-    protected $table = 'emojies';
-
-    /**
-     * 该模型是否被自动维护时间戳.
-     *
-     * @var bool
-     */
-    public $timestamps = true;
-
-    /**
-     * 存储时间戳的字段名
-     *
-     * @var string
-     */
-    const UPDATED_AT = 'updated_at';
-
-
-    /**
-     * 模型的「启动」方法.
-     *
-     * @return void
-     */
-    public static function boot()
-    {
-        parent::boot();
-    }
 }
