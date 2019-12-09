88970f497cadcc3e8afda81ae061f6406e32e708
ldd <dde-mail@qq.com>
2019-12-09 20:22:10 +0800

    [fixed] joind_at -> joined_at

---
+++ app/Api/Exceptions/UploadVerifyExceptionHandler.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Api\Exceptions;
+
+use Discuz\Http\Exception\UploadVerifyException;
+use Exception;
+use Tobscure\JsonApi\Exception\Handler\ExceptionHandlerInterface;
+use Tobscure\JsonApi\Exception\Handler\ResponseBag;
+
+class UploadVerifyExceptionHandler implements ExceptionHandlerInterface
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    public function manages(Exception $e)
+    {
+        return $e instanceof UploadVerifyException;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function handle(Exception $e)
+    {
+        $status = 500;
+        $error = [
+            'status' => (string) $status,
+            'code' => $e->getMessage() ?: 'file_not_allow'
+        ];
+        return new ResponseBag($status, [$error]);
+    }
+}
