3111ce6d8bf0cded98fb2453672d5bad9e8c0c56
Harvey <619596123@qq.com>
2019-12-13 17:07:24 +0800

    修改站点设置设置和获取接口

--- app/Api/Serializer/ForumSettingSerializer.php
+++ app/Api/Serializer/ForumSettingSerializer.php
@@ -7,7 +7,6 @@
 
 namespace App\Api\Serializer;
 
-use App\Models\Post;
 use App\Models\Thread;
 use App\Models\User;
 use Discuz\Api\Serializer\AbstractSerializer;
@@ -43,40 +42,88 @@ class ForumSettingSerializer extends AbstractSerializer
     public function getDefaultAttributes($model)
     {
         $attributes = [
-            'siteMode' => $this->settings->get('site_mode'), //pay public
-            'logo' => $this->logo($this->settings->get('logo')),
-            'siteName' => $this->settings->get('site_name'),
-            'siteIntroduction' => $this->settings->get('site_introduction'),
-            'siteStat' => $this->settings->get('site_stat'),
-            'siteRecord' => $this->settings->get('site_record'),
-            'sitePrice' => $this->settings->get('site_price'),
-            'siteExpire' => $this->settings->get('site_expire'),
-            'siteAuthorScale' => $this->settings->get('site_author_scale'),
-            'siteMasterScale' => $this->settings->get('site_master_scale'),
-            'siteInstall' => $this->settings->get('site_install'),
+            // 站点设置
+            'setsite' => [
+                'site_name' => $this->settings->get('site_name'),
+                'site_introduction' => $this->settings->get('site_introduction'),
+                'site_mode' => $this->settings->get('site_mode'), //pay public
+                'site_price' => $this->settings->get('site_price'),
+                'site_expire' => $this->settings->get('site_expire'),
+                'site_author_scale' => $this->settings->get('site_author_scale'),
+                'site_master_scale' => $this->settings->get('site_master_scale'),
+                'site_icp' => $this->settings->get('site_icp'),
+                'site_stat' => $this->settings->get('site_stat'),
+                'site_close' => (bool)$this->settings->get('site_close'),
+                'site_close_msg' => $this->settings->get('site_close_msg'),
+                'site_author' => User::where('id', $this->settings->get('site_author'))->first(['id', 'username']),
+                'site_logo' => $this->logo($this->settings->get('site_logo')),
+                'site_install' => $this->settings->get('site_install'),
+            ],
+
+            // 注册设置
+            'setreg' => [
+                'register_close' => (bool)$this->settings->get('register_close'),
+                'password_length' => (int)$this->settings->get('password_length'),
+                'password_strength' => $this->settings->get('password_strength'),
+            ],
+
+            // 第三方登陆设置
+            'passport' => [
+                // - 微信 h5
+                'offiaccount_close' => $this->settings->get('offiaccount_close', 'wx_offiaccount'),
+                'offiaccount_app_id' => $this->settings->get('offiaccount_app_id', 'wx_offiaccount'),
+                'offiaccount_app_secret' => $this->settings->get('offiaccount_app_secret', 'wx_offiaccount'),
+                // - 微信小程序
+                'miniprogram_close' => $this->settings->get('miniprogram_close', 'wx_miniprogram'),
+                'miniprogram_app_id' => $this->settings->get('miniprogram_app_id', 'wx_miniprogram'),
+                'miniprogram_app_secret' => $this->settings->get('miniprogram_app_secret', 'wx_miniprogram'),
+                // - 微信 pc
+                'oplatform_close' => $this->settings->get('oplatform_close', 'wx_oplatform'),
+                'oplatform_app_id' => $this->settings->get('oplatform_app_id', 'wx_oplatform'),
+                'oplatform_app_secret' => $this->settings->get('oplatform_app_secret', 'wx_oplatform'),
+            ],
+
+            // 支付设置
+            'paycenter' => [
+                'wxpay_close' => $this->settings->get('wxpay_close', 'wxpay'),
+                'wxpay_mch_id' => $this->settings->get('wxpay_mch_id', 'wxpay'),
+                'wxpay_app_id' => $this->settings->get('wxpay_app_id', 'wxpay'),
+                'wxpay_app_key' => $this->settings->get('wxpay_app_key', 'wxpay'),
+                'wxpay_app_secret' => $this->settings->get('wxpay_app_secret', 'wxpay'),
+            ],
+
+            // 附件设置
+            'setattach' => [
+                'support_img_ext' => $this->settings->get('support_img_ext'),
+                'support_file_ext' => $this->settings->get('support_file_ext'),
+                'support_max_size' => (int)$this->settings->get('support_max_size', 'default', ini_get('upload_max_filesize')),
+            ],
+
+            // 腾讯云设置
+            'qcloud' => [
+                'qcloud_close' => (bool)$this->settings->get('qcloud_close', 'qcloud'),
+                'qcloud_app_id' => $this->settings->get('qcloud_app_id', 'qcloud'),
+                'qcloud_secret_id' => $this->settings->get('qcloud_secret_id', 'qcloud'),
+                'qcloud_secret_key' => $this->settings->get('qcloud_secret_key', 'qcloud'),
+                'qcloud_token' => $this->settings->get('qcloud_token', 'qcloud'),
+                'qcloud_cms_image' => (bool)$this->settings->get('qcloud_cms_image', 'qcloud'),
+                'qcloud_cms_text' => (bool)$this->settings->get('qcloud_cms_text', 'qcloud'),
+                'qcloud_sms' => (bool)$this->settings->get('qcloud_sms', 'qcloud'),
+                'qcloud_sms_template_id' => $this->settings->get('qcloud_sms_template_id', 'qcloud'),
+                'qcloud_sms_sign' => $this->settings->get('qcloud_sms_sign', 'qcloud'),
+            ],
+
+            // 提现设置
+            'setcash' => [
+                'cash_interval_time' => $this->settings->get('cash_interval_time', 'cash'),
+                'cash_rate' => $this->settings->get('cash_rate', 'cash'),
+                'cash_min_sum' => $this->settings->get('cash_min_sum', 'cash'),
+                'cash_max_sum' => $this->settings->get('cash_max_sum', 'cash'),
+                'cash_sum_limit' => $this->settings->get('cash_sum_limit', 'cash'),
+            ],
+
             'threads' => Thread::count(),
             'members' => User::count(),
-            'siteAuthor' => User::where('id', $this->settings->get('site_author'))->first(['id', 'username']),
-            'passwordLength' => (int)$this->settings->get('password_length'),
-            'passwordStrength' => $this->settings->get('password_strength'),
-            'allowRegister' => (bool)$this->settings->get('allow_register'),
-            'siteClose' => (bool)$this->settings->get('site_close'),
-            'siteCloseMsg' => $this->settings->get('site_close_msg'),
-            'supportImgExt' => $this->settings->get('support_img_ext'),
-            'supportFileExt' => $this->settings->get('support_file_ext'),
-            'supportMaxSize' => (int)$this->settings->get('support_max_size', 'default', ini_get('upload_max_filesize')),
-            'qcloud' => (bool)$this->settings->get('qcloud'),
-            'qcloudCmsImage' => (bool)$this->settings->get('qcloud_cms_image'),
-            'qcloudCmsText' => (bool)$this->settings->get('qcloud_cms_text'),
-            'qcloudSms' => (bool)$this->settings->get('qcloud_sms'),
-            'qcloudSecretId' => $this->decrypt($this->settings->get('secretId', 'qcloud')),
-            'qcloudSecretKey' => $this->decrypt($this->settings->get('secretKey', 'qcloud')),
-            'qcloudToken' => $this->decrypt($this->settings->get('token', 'qcloud', '')),
-
-            // 登陆配置
-            'wechat_h5'     => (bool) $this->settings->get('wechat_h5', 'wechat_h5'),
-            'wechat_min'    => (bool) $this->settings->get('wechat_min', 'wechat_min'),
-            'wechat_pc'     => (bool) $this->settings->get('wechat_pc', 'wechat_pc'),
         ];
 
         if ($this->actor->exists) {
@@ -85,8 +132,6 @@ class ForumSettingSerializer extends AbstractSerializer
                 'registerTime' => $this->formatDate($this->actor->created_at),
             ];
         }
-
-
 
         return $attributes;
     }
