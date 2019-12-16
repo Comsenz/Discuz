8f66917537764d0109544d4889c5f91a46ae8973
gudanxin1314 <56377852+gudanxin1314@users.noreply.github.com>
2019-12-16 10:08:57 +0800

    上传附件和图片以及回复主题

--- resources/frame/src/template/default/view/m_site/themeDetails/replyToTopicView.vue
+++ resources/frame/src/template/default/view/m_site/themeDetails/replyToTopicView.vue
@@ -1,22 +1,4 @@
 <template>
-    <!-- <div class="reply-to-topic-box post-topic-box">
-       <header class="post-topic-header">
-         <span @click="backClick"  class="icon iconfont icon-back post-topic-header-icon" ></span>
-         <h2 class="postHeadTit">{{headerTitle}}</h2>
-         <van-button type="primary" size="mini" @click="publish">发布</van-button>
-       </header>
-      <main class="reply-to-topic-main">
-        <textarea class="reply-box" name="reply-to-topic-text" id="reply-to-topic-main-text" placeholder="请输入内容" ></textarea>
-      </main>
-
-      <footer class="reply-to-topic-footer">
-        <div class="reply-to-topic-footer-left">
-          <span  class="icon iconfont icon-label reply-to-topic-header-icon" ></span>
-          <span  class="icon iconfont icon-picture reply-to-topic-header-icon" ></span>
-        </div>
-      </footer>
-    </div> -->
-
     <div class="post-topic-box">
       <header class="post-topic-header">
         <span @click="backClick"  class="icon iconfont icon-back post-topic-header-icon" ></span>
@@ -25,18 +7,21 @@
       </header>
 
       <div class="post-topic-form">
-        <!-- <textarea placeholder="评论" v-model="shareText" ref="shareTextArea" @focus="showFacePanel = false"></textarea> -->
-        <textarea class="reply-box" id="post-topic-form-text" name="post-topic" ref="textarea"  placeholder="请输入内容" v-model="replyText" :maxlength="keywordsMax" @change="searchChange"@focus="showFacePanel = false"></textarea>
-        <div class="uploadBox">
-          <van-uploader v-model="fileList" multiple />
+        <!-- <textarea class="reply-box" id="post-topic-form-text" name="post-topic" ref="textarea"  placeholder="请输入内容" v-model="replyText" :maxlength="keywordsMax" @change="searchChange"@focus="showFacePanel = false"></textarea> -->
+         <textarea class="reply-box" id="post-topic-form-text" name="post-topic" ref="textarea"  placeholder="请输入内容" v-model="replyText" :maxlength="keywordsMax" @change="searchChange"@focus="showFacePanel = false;footMove = false;keyboard = false;"></textarea>
+
+        <div class="uploadBox" v-show="uploadShow">
+          <van-uploader :max-count="12" :after-read="handleFile" :delete="deleteFile" accept="image/*"  v-model="fileList">
+          </van-uploader>
         </div>
       </div>
 
       <footer class="post-topic-footer" :class="{'footMove':footMove}">
-        <div class="post-topic-footer-left">
+        <div class="post-topic-footer-left reply-topic-footer-left">
           <span  class="icon iconfont icon-label post-topic-header-icon" :class="{'icon-keyboard':keyboard}" @click="addExpression"></span>
-           <span  class="icon iconfont icon-picture post-topic-header-icon" ></span>
-          <!-- <span  class="icon iconfont icon-enclosure post-topic-header-icon" ></span> -->
+           <span  class="icon iconfont icon-picture post-topic-header-icon uploadIcon">
+             <input type="file" accept="image/*" @change="handleFileUp" class="hiddenInput"/>
+           </span>
         </div>
       </footer>
       <Expression :faceData="faceData" @onFaceChoose="handleFaceChoose" v-if="showFacePanel" class="expressionBox"></Expression>
