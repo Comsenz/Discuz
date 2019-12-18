4c9f652ec9ef6a7546cd734185b3b4ed5f6aa629
gudanxin1314 <56377852+gudanxin1314@users.noreply.github.com>
2019-12-18 18:10:58 +0800

    vant新增图片预览组件

--- resources/frame/src/extend/viewBase/vantuiInit.js
+++ resources/frame/src/extend/viewBase/vantuiInit.js
@@ -1,5 +1,5 @@
 import Vue from 'vue';
-import { Picker,Button,Popup,Cell,Tab,Tabs,Field,Divider,CellGroup,Search,NumberKeyboard,Checkbox,CheckboxGroup,Toast,Icon,uploader,Swipe,SwipeItem,Row,Col } from 'vant';
+import { Picker,Button,Popup,Cell,Tab,Tabs,Field,Divider,CellGroup,Search,NumberKeyboard,Checkbox,CheckboxGroup,Toast,Icon,uploader,Swipe,SwipeItem,Row,Col,Image,ImagePreview } from 'vant';
 
 
-Vue.use(Picker).use(Icon).use(Toast).use(Button).use(Popup).use(Cell).use(Tab).use(Tabs).use(Field).use(Divider).use(CellGroup).use(Search).use(NumberKeyboard).use(Checkbox).use(CheckboxGroup).use(uploader).use(Swipe).use(SwipeItem).use(Row).use(Col);
+Vue.use(Picker).use(Icon).use(Toast).use(Button).use(Popup).use(Cell).use(Tab).use(Tabs).use(Field).use(Divider).use(CellGroup).use(Search).use(NumberKeyboard).use(Checkbox).use(CheckboxGroup).use(uploader).use(Swipe).use(SwipeItem).use(Row).use(Col).use(Image).use(ImagePreview);
