55c874eaa308e3365ae32e3f6ef897424cdd84d1
ldd <dde-mail@qq.com>
2019-12-24 11:49:12 +0800

    [add] post_mod

---
+++ app/Models/PostMod.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * Discuz & Tencent Cloud
+ * This is NOT a freeware, use is subject to license terms
+ */
+
+namespace App\Models;
+
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+/**
+ * @property int $post_id
+ * @property string $stop_word
+ * @package App\Models
+ */
+class PostMod extends Model
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected $table = 'post_mod';
+
+    /**
+     * {@inheritdoc}
+     */
+    public $timestamps = false;
+
+    /**
+     * Define the relationship with the post's mod stop words.
+     *
+     * @return BelongsTo
+     */
+    public function post()
+    {
+        return $this->belongsTo(Post::class);
+    }
+}
