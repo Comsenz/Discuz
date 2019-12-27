2e3ce83c52fc2ecf12ea39b3de16fd83e4a665f9
gynnnn <920791506@qq.com>
2019-12-27 19:08:23 +0800

    更新

--- resources/frame/src/template/default/viewConfig/tplConfig.js
+++ resources/frame/src/template/default/viewConfig/tplConfig.js
@@ -528,6 +528,12 @@ export default {
     }
   } else {
     console.log('前台未登录，跳转');
+
+    if (!noLoginAccessPage.includes(to.name)){
+      next({path:'/'});
+      return;
+    }
+
     next();
     return;
   }
