bd002eb176a8c4ace7a95dca173b1c16cfaf3aa7
YangPengPc <1324901147@qq.com>
2019-12-31 17:56:51 +0800

    圣诞测试版-测试微信支付0.12

--- resources/frame/src/template/default/viewConfig/tplConfig.js
+++ resources/frame/src/template/default/viewConfig/tplConfig.js
@@ -16,14 +16,6 @@ export default {
     m_site:{
       css: ['/css/reset.css'],
       // js: ['/js/rem.js'],
-      'pay-status':{
-        comLoad:function (resolve) {
-          require(['../view/m_site/common/pay/payView'],resolve)
-        },
-        metaInfo:{
-          title:"支付订单查询"
-        }
-      },
       'circle':{
         comLoad:function (resolve) {
           require(['../view/m_site/home/circleView'],resolve)
@@ -436,7 +428,15 @@ export default {
         metaInfo:{
           title:"我的钱包"
         }
-      }
+      },
+      'pay-status':{
+        comLoad:function (resolve) {
+          require(['../view/m_site/common/pay/payView'],resolve)
+        },
+        metaInfo:{
+          title:"支付订单查询"
+        }
+      },
 
 
     }
