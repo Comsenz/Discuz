875c2d7bd03518fa9e23b4fd31f874e4fb0917cd
gudanxin1314 <56377852+gudanxin1314@users.noreply.github.com>
2020-01-08 16:44:15 +0800

    打包测试

--- resources/frame/src/template/default/view/m_site/home/postTopicView.vue
+++ resources/frame/src/template/default/view/m_site/home/postTopicView.vue
@@ -45,13 +45,13 @@
             <span  class="icon iconfont icon-label post-topic-header-icon" :class="{'icon-keyboard':keyboard}" @click="addExpression"></span>
             <span  class="icon iconfont icon-picture post-topic-header-icon uploadIcon" v-if="canUploadImages && limitMaxLength">
               <input type="file" @change="handleFileUp" class="hiddenInput" v-if="isAndroid && isWeixin"/>
-              <input type="file" :accept="supportImgExtRes" @change="handleFileUp" class="hiddenInput" v-else="" mutiple="mutiple" capture="camera"/>
+              <input type="file" :accept="supportImgExtRes" @change="handleFileUp" class="hiddenInput" v-else="" mutiple="mutiple"/>
             </span>
             <span  class="icon iconfont icon-picture post-topic-header-icon uploadIcon" v-else="" @click="beforeHandleFile">
             </span>
             <span class="icon iconfont icon-enclosure post-topic-header-icon uploadIcon" :class="{'hide': encuploadShow}" v-if="canUploadAttachments && limitMaxEncLength">
               <input type="file" v-if="isAndroid && isWeixin" @change="handleEnclosure" class="hiddenInput"/>
-              <input type="file" v-else="" :accept="supportFileExt" @change="handleEnclosure" class="hiddenInput" capture="camera"/>
+              <input type="file" v-else="" :accept="supportFileExt" @change="handleEnclosure" class="hiddenInput"/>
             </span>
             <span  class="icon iconfont icon-enclosure post-topic-header-icon uploadIcon":class="{'hide': encuploadShow}" v-else="" @click="beforeHandleEnclosure">
             </span>
