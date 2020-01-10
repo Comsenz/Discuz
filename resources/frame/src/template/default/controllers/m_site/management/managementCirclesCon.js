744b242043fe66d35ff6bcda0499facc08383b0c
gudanxin1314 <56377852+gudanxin1314@users.noreply.github.com>
2020-01-10 17:50:44 +0800

    侧边栏根据权限判断是否显示站点管理

--- resources/frame/src/template/default/controllers/m_site/management/managementCirclesCon.js
+++ resources/frame/src/template/default/controllers/m_site/management/managementCirclesCon.js
@@ -5,15 +5,16 @@
 export default {
 	data: function() {
 		return {
-			// headOneShow: false,
-			// navShow: false,
-			// headTwoShow: true
+			canHideThreads:false,
+			canEditUserGroup:false,
+			canCreateInvite:false
 		}
 	},
 	 //用于数据初始化
     created: function(){
 		// console.log(this.headOneShow)
-		this.managementCircles()
+		this.managementCircles();
+    this.getInfo();
 	},
 	methods: {
 	    //跳转到成员管理
@@ -34,26 +35,50 @@ export default {
 	    // }
 
 	// },
-	managementCircles(str){
-		switch (str) {
-		  case 'members-management':
-			this.$router.push('/members-management'); //成员管理
-			break;
-		  case 'delete':
-			this.$router.push('/delete'); //批量管理
-			break;
-			case 'invite-join':
-			this.$router.push('/invite-join'); //成员邀请
-			break;
-		  default:
-			// this.$router.push('/');
-		}
+    managementCircles(str){
+      switch (str) {
+        case 'members-management':
+        this.$router.push('/members-management'); //成员管理
+        break;
+        case 'delete':
+        this.$router.push('/delete'); //批量管理
+        break;
+        case 'invite-join':
+        this.$router.push('/invite-join'); //成员邀请
+        break;
+        default:
+        // this.$router.push('/');
+      }
 	  },
+    getInfo() {
+        //请求站点信息，用于判断站点是否是付费站点
+        this.appFetch({
+          url: 'forum',
+          method: 'get',
+          data: {
+          }
+        }).then((res) => {
+          if (res.errors){
+              this.$toast.fail(res.errors[0].code);
+              throw new Error(res.error)
+            } else {
+             // console.log(res);
+             this.canHideThreads = res.readdata._data.canHideThreads;
+             this.canEditUserGroup = res.readdata._data.canEditUserGroup;
+             this.canCreateInvite = res.readdata._data.canCreateInvite;
+             // console.log(this.canHideThreads,this.canEditUserGroup,this.canCreateInvite);
+             //判断 当用户组拥有批量删除帖子、管理-邀请加入、编辑用户组、编辑用户组状态这4个权限中的任意一项时才会显示该菜单
+             if(this.canHideThreads || this.canEditUserGroup || this.canCreateInvite){
+               // this.sidebarList2.splice(1,1);
+             }
+           }
+        });
+      },
 	},
-	
+
 
 	mounted: function() {
-		
+
 	},
 	beforeRouteLeave (to, from, next) {
 	   next()
