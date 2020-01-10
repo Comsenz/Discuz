974bf440cf84ff39356be8be5001ed6e1ee25e7c
Harvey <619596123@qq.com>
2020-01-10 17:26:08 +0800

    提交语言包

--- resources/lang/zh-CN/setting.php
+++ resources/lang/zh-CN/setting.php
@@ -9,12 +9,12 @@ return [
     // default
     'set_error' => '设值失败',
 
-    'min_greater_than_max' => '单次提现最小金额 不能大于最大金额',
-    'min_greater_than_limit' => '单次提现最小金额 不能大于单日提现总金额',
-    'min_exceed_5000' => '单次提现最小金额 不能超过5000',
+    'min_greater_than_max' => '单次提现最小金额 不能大于 最大金额',
+    'min_greater_than_limit' => '单次提现最小金额 不能大于 单日提现总金额',
+    'min_exceed_5000' => '单次提现最小金额 不能超过 5000',
 
-    'max_greater_than_limit' => '单次提现最大金额 不能大于单日提现总金额',
-    'max_exceed_5000' => '单次提现最大金额 不能超过5000',
+    'max_greater_than_limit' => '单次提现最大金额 不能大于 单日提现总金额',
+    'max_exceed_5000' => '单次提现最大金额 不能超过 5000',
 
-    'limit_exceed_5000' => '单日提现总金额 不能超过5000',
+    'limit_exceed_5000' => '单日提现总金额 不能超过 5000',
 ];
