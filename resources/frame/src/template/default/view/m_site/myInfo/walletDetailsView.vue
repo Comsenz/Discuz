beca3a5ca3ac54262ac97995a4863ec24cee88ab
YangPengPc <1324901147@qq.com>
2020-01-10 21:04:11 +0800

    修改bug：9622

--- resources/frame/src/template/default/view/m_site/myInfo/walletDetailsView.vue
+++ resources/frame/src/template/default/view/m_site/myInfo/walletDetailsView.vue
@@ -10,7 +10,11 @@
     >
     <van-pull-refresh v-model="isLoading" @refresh="onRefresh">
       <main class="content">
-        <Panenl :title="type[item.attributes.change_type]" :num="item.attributes.change_available_amount" v-for="(item,index) in walletDetailsList" :key="index">
+        <Panenl :title="type[item.attributes.change_type]"
+                :status="item.attributes.change_type === 30 || item.attributes.change_type === 31 || item.attributes.change_type === 32?true:false"
+                :num="item.attributes.change_type === 30 || item.attributes.change_type === 31 || item.attributes.change_type === 32?'+' + item.attributes.change_available_amount : item.attributes.change_available_amount"
+                v-for="(item,index) in walletDetailsList"
+                :key="index">
           <!-- <span slot="label">当前可用金额：{{item.attributes.change_freeze_amount}}</span> -->
           <span slot="label">{{$moment(item.attributes.created_at).format('YYYY-MM-DD HH:mm')}}</span>
         </Panenl>
@@ -28,7 +32,7 @@
         </Panenl> -->
       </main>
       <footer class="my-info-money-footer"></footer>
-</van-pull-refresh>    
+</van-pull-refresh>
   </van-list>
     </div>
 </template>
--- resources/frame/src/template/default/view/m_site/myInfo/withdrawalsRecordView.vue
+++ resources/frame/src/template/default/view/m_site/myInfo/withdrawalsRecordView.vue
@@ -11,40 +11,12 @@
     >
     <van-pull-refresh v-model="isLoading" @refresh="onRefresh">
       <main class="withdrawals-record-main content" >
-        <Panenl :title='cashStatusObj[item.attributes.cash_status]' :num='item.attributes.cash_apply_amount' v-for="(item,index) in withdrawalsList" :key="index"  v-if="withdrawalsList.length>0">
+        <Panenl :title='cashStatusObj[item.attributes.cash_status]' :num="'-'+item.attributes.cash_apply_amount" v-for="(item,index) in withdrawalsList" :key="index"  v-if="withdrawalsList.length>0">
           <span slot="label">流水号：{{item.attributes.cash_sn}}</span>
           <span slot="label">{{$moment(item.attributes.created_at).format('YYYY-MM-DD HH:mm')}}</span>
         </Panenl>
-        <!-- <Panenl title="审核不通过，原因：使用非正常手段获取利益" num="-1000.00">
-          <span slot="label">流水号：34927384234</span>
-          <span slot="label">2019-08-10 20:30</span>
-        </Panenl>
-        <Panenl title="审核通过，打款中" num="-1000.00">
-          <span slot="label">流水号：34927384234</span>
-          <span slot="label">2019-08-10 20:30</span>
-        </Panenl>
-        <Panenl title="审核通过，打款中" num="-1000.00">
-          <span slot="label">流水号：34927384234</span>
-          <span slot="label">2019-08-10 20:30</span>
-        </Panenl>
-        <Panenl title="审核通过，打款中" num="-1000.00">
-          <span slot="label">流水号：34927384234</span>
-          <span slot="label">2019-08-10 20:30</span>
-        </Panenl>
-        <Panenl title="审核通过，打款中" num="-1000.00">
-          <span slot="label">流水号：34927384234</span>
-          <span slot="label">2019-08-10 20:30</span>
-        </Panenl>
-        <Panenl title="审核通过，打款中" num="-1000.00">
-          <span slot="label">流水号：34927384234</span>
-          <span slot="label">2019-08-10 20:30</span>
-        </Panenl>
-        <Panenl title="审核通过，打款中" num="-1000.00">
-          <span slot="label">流水号：34927384234</span>
-          <span slot="label">2019-08-10 20:30</span>
-        </Panenl> -->
       </main>
-</van-pull-refresh>    
+</van-pull-refresh>
   </van-list>
       <footer class="my-info-money-footer"></footer>
     </div>
