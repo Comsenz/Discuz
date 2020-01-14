811cc60d02295e064f9be405e20aabd5cdf71725
zz <583417300@qq.com>
2020-01-14 14:41:06 +0800

    打赏金额分成后大于零时才添加钱包明细

--- app/Commands/Trade/Notify/NotifyTrait.php
+++ app/Commands/Trade/Notify/NotifyTrait.php
@@ -40,7 +40,6 @@ trait NotifyTrait
                 return $order_info;
             } elseif ($order_info->type == Order::ORDER_TYPE_REWARD) {
                 //打赏
-
                 $order_amount = $order_info->amount;//订单金额
                 //站长作者分成配置
                 $site_author_scale = $setting->get('site_author_scale');
@@ -57,30 +56,32 @@ trait NotifyTrait
                     $payee_amount = $order_amount;
                 }
 
-                //收款人钱包可用金额增加
-                $payee_id                      = $order_info->payee_id; //收款人
-                $user_wallet                   = UserWallet::lockForUpdate()->find($payee_id);
-                $user_wallet->available_amount = $user_wallet->available_amount + $payee_amount;
-                $user_wallet->save();
-                //收款人钱包明细记录
-                $payee_change_available_amount = $payee_amount;
-                $change_type                   = '';
-                $change_type_lang              = '';
-                if ($order_info->type = Order::ORDER_TYPE_REWARD) {
-                    //打赏收入
-                    $change_type      = UserWalletLog::TYPE_INCOME_REWARD;
-                    $change_type_lang = 'wallet.reward_income';
+                if ($payee_amount > 0) {
+                    //收款人钱包可用金额增加
+                    $payee_id                      = $order_info->payee_id; //收款人
+                    $user_wallet                   = UserWallet::lockForUpdate()->find($payee_id);
+                    $user_wallet->available_amount = $user_wallet->available_amount + $payee_amount;
+                    $user_wallet->save();
+                    //收款人钱包明细记录
+                    $payee_change_available_amount = $payee_amount;
+                    $change_type                   = '';
+                    $change_type_lang              = '';
+                    if ($order_info->type = Order::ORDER_TYPE_REWARD) {
+                        //打赏收入
+                        $change_type      = UserWalletLog::TYPE_INCOME_REWARD;
+                        $change_type_lang = 'wallet.reward_income';
+                    }
+                    //添加钱包明细
+                    $user_wallet_log = UserWalletLog::createWalletLog(
+                        $payee_id,
+                        $payee_change_available_amount,
+                        0,
+                        $change_type,
+                        app('translator')->get($change_type_lang),
+                        null,
+                        $order_info->id
+                    );
                 }
-                //添加钱包明细
-                $user_wallet_log = UserWalletLog::createWalletLog(
-                    $payee_id,
-                    $payee_change_available_amount,
-                    0,
-                    $change_type,
-                    app('translator')->get($change_type_lang),
-                    null,
-                    $order_info->id
-                );
                 return $order_info;
             }
         }
