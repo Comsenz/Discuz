804a6852703487dc8501736489488ddb0ef48416
xinghailong <122363686@qq.com>
2020-01-14 17:14:37 +0800

    20200114 增加注册收入统计

--- app/Api/Serializer/FinanceStatisticSerializer.php
+++ app/Api/Serializer/FinanceStatisticSerializer.php
@@ -24,12 +24,12 @@ class FinanceStatisticSerializer extends AbstractSerializer
     {
         return [
             'totalIncome'            => number_format(Arr::get($model, 'totalIncome', 0.00), 2),
-            'totalExpenditures'      => number_format(Arr::get($model, 'totalExpenditures', 0.00), 2),
             'totalWithdrawal'        => number_format(Arr::get($model, 'totalWithdrawal', 0.00), 2),
             'totalWallet'            => number_format(Arr::get($model, 'totalWallet', 0.00), 2),
             'totalProfit'            => number_format(Arr::get($model, 'totalProfit', 0.00), 2),
             'withdrawalProfit'       => number_format(Arr::get($model, 'withdrawalProfit', 0.00), 2),
             'orderRoyalty'           => number_format(Arr::get($model, 'orderRoyalty', 0.00), 2),
+            'totalRegisterProfit'    => number_format(Arr::get($model, 'totalRegisterProfit', 0.00), 2),
             'orderCount'             => Arr::get($model, 'orderCount', 0),
         ];
     }
