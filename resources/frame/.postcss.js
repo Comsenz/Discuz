2eea7dbedbd590fe7c845abb2b1b84e46ac226eb
gudanxin1314 <56377852+gudanxin1314@users.noreply.github.com>
2020-01-17 19:29:56 +0800

    打包测试

--- resources/frame/.postcss.js
+++ resources/frame/.postcss.js
@@ -14,17 +14,17 @@ module.exports = {
       minPixelValue: 0
     }
     // 'autoprefixer': {overrideBrowserslist: [
-    //   'Android 4.1',
-    //   'iOS 7.1',
-    //   'Chrome > 31',
-    //   'ff > 31',
-    //   'ie >= 8'
-    //   ]
-    // },
-    // 'postcss-pxtorem': {
-    //   rootValue: 37.5,
-    //   propList: ['*']
+    //   'Android 4.1',        
+    //   'iOS 7.1',        
+    //   'Chrome > 31',        
+    //   'ff > 31',        
+    //   'ie >= 8'      
+    //   ]    
+    // },    
+    // 'postcss-pxtorem': {      
+    //   rootValue: 37.5,      
+    //   propList: ['*']    
     // }
-
+  
   }
 }
