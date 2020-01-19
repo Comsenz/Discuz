dca71bb67a4af14f25bddbc88396352edf4bff97
xinghailong <122363686@qq.com>
2020-01-19 18:27:52 +0800

    20200119  临时返回   非管理员 前台需要的字段

--- app/Api/Serializer/ForumSettingSerializer.php
+++ app/Api/Serializer/ForumSettingSerializer.php
@@ -116,18 +116,18 @@ class ForumSettingSerializer extends AbstractSerializer
                     'siteAuthorScale' => $this->settings->get('site_author_scale'),
                     'siteMasterScale' => $this->settings->get('site_master_scale'),
                 ];
-            } else {
-                //@todo 临时返回 非管理员 前台需要的字段
-                $attributes['setsite'] = $this->getSiteSettings();
-                $attributes['qcloud']['qcloud_close'] = (bool)$this->settings->get('qcloud_close', 'qcloud');
-                $attributes['qcloud']['qcloud_cms_image'] = (bool)$this->settings->get('qcloud_cms_image', 'qcloud');
-                $attributes['qcloud']['qcloud_cms_text'] = (bool)$this->settings->get('qcloud_cms_text', 'qcloud');
-                $attributes['qcloud']['qcloud_sms'] = (bool)$this->settings->get('qcloud_sms', 'qcloud');
-                $attributes['passport']['offiaccount_close'] = $this->settings->get('offiaccount_close', 'wx_offiaccount');
-                $attributes['passport']['miniprogram_close'] = $this->settings->get('miniprogram_close', 'wx_miniprogram');
-                $attributes['passport']['oplatform_close'] = $this->settings->get('oplatform_close', 'wx_oplatform');
             }
         }
+
+        //@todo 临时返回 非管理员 前台需要的字段
+        $attributes['setsite'] = $this->getSiteSettings();
+        $attributes['qcloud']['qcloud_close'] = (bool)$this->settings->get('qcloud_close', 'qcloud');
+        $attributes['qcloud']['qcloud_cms_image'] = (bool)$this->settings->get('qcloud_cms_image', 'qcloud');
+        $attributes['qcloud']['qcloud_cms_text'] = (bool)$this->settings->get('qcloud_cms_text', 'qcloud');
+        $attributes['qcloud']['qcloud_sms'] = (bool)$this->settings->get('qcloud_sms', 'qcloud');
+        $attributes['passport']['offiaccount_close'] = $this->settings->get('offiaccount_close', 'wx_offiaccount');
+        $attributes['passport']['miniprogram_close'] = $this->settings->get('miniprogram_close', 'wx_miniprogram');
+        $attributes['passport']['oplatform_close'] = $this->settings->get('oplatform_close', 'wx_oplatform');
 
         return $attributes;
     }
