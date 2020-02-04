38163eba9536746b8a7ad38c0ed18cea22bd810f
Harvey <619596123@qq.com>
2020-02-04 20:05:23 +0800

    完成单条、批量内容置顶通知

--- app/MessageTemplate/PostOrderMessage.php
+++ app/MessageTemplate/PostOrderMessage.php
@@ -9,6 +9,7 @@ namespace App\MessageTemplate;
 
 use Discuz\Foundation\Application;
 use Discuz\Notifications\Messages\DatabaseMessage;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Str;
 
 class PostOrderMessage extends DatabaseMessage
@@ -27,9 +28,19 @@ class PostOrderMessage extends DatabaseMessage
 
     protected function getContent($data)
     {
+        /**
+         * 判断是否有主题标题
+         * 使用首贴内容代替 并过滤 内容的标签
+         */
+        $thread = Arr::get($data, 'message', false);
+        $message = '';
+        if ($thread) {
+            $message = empty($thread->title) ? $message = $thread->firstPost->content : $thread->title;
+        }
+
         return $this->translator->get('core.post_order_change_detail', [
             'user' => $this->notifiable->username,
-            'message' => Str::words($data['message'], 10)
+            'message' => Str::words($message, 10)
         ]);
     }
 }
