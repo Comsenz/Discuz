16302b5873aca6a874b3287fec25667cb63e104e
ldd <dde-mail@qq.com>
2020-02-10 17:17:11 +0800

    [add] 支付密码

--- app/Commands/Trade/PayOrder.php
+++ app/Commands/Trade/PayOrder.php
@@ -104,11 +104,12 @@ class PayOrder
                 'required_if:payment_type,20',
                 function ($attribute, $value, $fail) {
                     // 使用用户钱包支付时，检查密码
-                    if (
-                        $this->data->get('payment_type') == 20
-                        && ! $this->actor->checkWalletPayPassword($value)
-                    ) {
-                        $fail('支付密码错误。');
+                    if ($this->data->get('payment_type') == 20) {
+                        if (empty($this->actor->pay_password)) {
+                            $fail('未设置支付密码');
+                        } elseif (! $this->actor->checkWalletPayPassword($value)) {
+                            $fail('支付密码错误');
+                        }
                     }
                 }
             ],
