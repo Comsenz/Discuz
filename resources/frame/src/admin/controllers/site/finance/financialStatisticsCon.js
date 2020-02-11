7160fc8c8e67d504f159e1aa079d4875b2c61703
gyn <920791506@qq.com>
2020-02-11 09:57:44 +0800

    更新

---
+++ resources/frame/src/admin/controllers/site/finance/financialStatisticsCon.js
@@ -0,0 +1,42 @@
+export default {
+    data:function(){
+        return{
+            financialList:[
+                {
+                    title:"用户总充值",
+                    num:"20,187"
+                },
+                {
+                    title:"用户总充提现",
+                    num:"20,187"
+                },
+                {
+                    title:"用户钱包总金额",
+                    num:"20,187"
+                },
+                {
+                    title:"用户订单总数",
+                    num:"20,187"
+                },
+                {
+                    title:"平台总盈利",
+                    num:"20,187"
+                },
+                {
+                    title:"提现手续费收入",
+                    num:"20,187"
+                },
+                {
+                    title:"打赏提成收入",
+                    num:"20,187"
+                },
+                {
+                    title:"注册加入收入",
+                    num:"20,187"
+                },
+                
+            ]
+           
+        }
+    }
+}
---
+++ resources/frame/src/admin/view/site/finance/financialStatistics.vue
@@ -0,0 +1,22 @@
+<template>
+<div class="financial-box">
+<div class="financial-statistics">
+    <div class="financial" v-for="(item,index) in financialList" :key="index">
+        <div class="financial-head">
+        <div class="financial-title">{{item.title}}</div>
+        <span class=""></span>
+        <span class="financial-con">¥<span class="financial-num">{{item.num}}</span></span>
+        </div>
+    </div>
+</div>
+</div>
+
+</template>
+<script>
+import '../../../scss/site/module/financeStyle.scss';
+import financialStatisticsCon from '../../../controllers/site/finance/financialStatisticsCon';
+export default{
+    name:"financial-statistics-view",
+    ...financialStatisticsCon
+}
+</script>
