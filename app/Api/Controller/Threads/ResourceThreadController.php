6e6cb5f9cb54f020ff6b49334a20ecd57b7aabd0
ldd <dde-mail@qq.com>
2020-02-11 17:36:29 +0800

    [fixed] 尝试解决打赏列表问题

--- app/Api/Controller/Threads/ResourceThreadController.php
+++ app/Api/Controller/Threads/ResourceThreadController.php
@@ -119,8 +119,10 @@ class ResourceThreadController extends AbstractResourceController
         if (in_array('rewardedUsers', $include)) {
             $allRewardedUser = Order::with('user')
                 ->where('thread_id', $thread->id)
-                ->where('type', 2)
-                ->where('status', 1)
+                ->where('status', Order::ORDER_STATUS_PAID)
+                ->where('type', Order::ORDER_TYPE_REWARD)
+                ->orderBy('created_at', 'desc')
+                ->orderBy('id', 'desc')
                 ->get();
 
             $thread->setRelation('rewardedUsers', $allRewardedUser->pluck('user')->filter());
