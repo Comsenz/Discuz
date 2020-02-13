46a7a0d379a294ffdb0a1dbf1baf8a312abb15cb
gudanxin1314 <56377852+gudanxin1314@users.noreply.github.com>
2020-02-13 11:31:09 +0800

    分享标题

--- resources/frame/src/common/filters.js
+++ resources/frame/src/common/filters.js
@@ -4,11 +4,3 @@ Vue.filter('timeAgo',function(value){
   return moment().fromNow();  // ~~~时间之前
 })
 
-Vue.filter('filterHtml',{
-    read:function(val){//val就是以获取的msg的值
-        return val.replace(/<[^>]*>/g);//去除文字的<...></...>标签
-    },
-    write:function(){
-        return val;
-    }
-});
