7f0b70e7ec28c93933b880f4758220721820d100
gudanxin1314 <56377852+gudanxin1314@users.noreply.github.com>
2020-02-13 14:22:29 +0800

    关注功能微调

--- resources/frame/src/template/default/view/m_site/myInfo/myCareView.vue
+++ resources/frame/src/template/default/view/m_site/myInfo/myCareView.vue
@@ -7,7 +7,6 @@
             v-model="searchVal"
             placeholder="搜索我关注的人"
             background="#f8f8f8"
-            show-action
             @input="onSearch"
             @cancel="onCancel"
             class="searchCon"
@@ -15,12 +14,10 @@
         </form>
         <div class="searchRes" v-if="searchResStatus">
           <div class="resUser" v-for="(item, index) in searchUserList" :key="index">
-            <!-- <a href="javascript:;" class="alreadFollow">已关注</a> -->
            <img v-if="item.toUser && item.toUser._data.avatarUrl" :src="item.toUser._data.avatarUrl" class="resUserHead"  @click="jumpPerDet(item.toUser._data.id)">
             <img v-else="" :src="appConfig.staticBaseUrl+'/images/noavatar.gif'" class="resUserHead"  @click="jumpPerDet(item.toUser._data.id)">
             <div class="resUserDet">
                <span class="resUserName" v-html="item.toUser && item.toUser._data.username.replace(searchVal,'<i>'+searchVal+'</i>')" ></span>
-               <!-- <a href="javascript:;" class="followHe" v-if="item.toUser && item.toUser._data.follow == '' || item.toUser && item.toUser._data.follow == '0' || item.toUser && item.toUser._data.follow == null" @click="followSwitch()">关注TA</a> -->
                <a href="javascript:;" class="alreadFollow" v-if="item._data.is_mutual == '0'" @click="followSwitch(item._data.is_mutual,item.toUser._data.id,index)">已关注</a>
                <a href="javascript:;" class="alreadFollow" v-else-if="item._data.is_mutual == '1'" @click="followSwitch(item._data.is_mutual,item.toUser._data.id,index)">相互关注</a>
                <a href="javascript:;" class="followHe" v-else="item._data.is_mutual == '2'" @click="followSwitch('2',item.toUser._data.id,index)">关注TA</a>
