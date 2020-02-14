15ac5c542b3ac8e6ce2021c6d72a10831f1841a5
gyn <920791506@qq.com>
2020-02-14 10:16:45 +0800

    后台财务

--- resources/frame/src/admin/view/site/finance/financialStatistics.vue
+++ resources/frame/src/admin/view/site/finance/financialStatistics.vue
@@ -11,8 +11,26 @@
 </div>
 <div class="financial-profit">
     <div class="financial-profit-title">
-        <span class="iconfont iconchongzhi"></span>
-        <div class="financial-profit-titles">盈利统计</div>  
+        <div class='financial-profit-title-left'>
+            <span class="iconfont iconchongzhi"></span>
+            <span class="financial-profit-titles">盈利统计</span>
+        </div>
+        <div class='financial-profit-title-right'>
+            <span>今日</span>
+            <span>近月</span>
+            <span>今年</span>
+            <el-date-picker
+                v-model="financialTime"
+                clearable
+                type="daterange"
+                value-format="yyyy-MM-dd"
+                range-separator="至"
+                start-placeholder="开始日期"
+                end-placeholder="结束日期"
+                :picker-options="pickerOptions">
+            </el-date-picker>
+        </div>
+
     </div>
     <div class="financial-profit-chart" ref="financialProfitEcharts">     
     </div>
@@ -21,7 +39,21 @@
 <div class="financial-order">
     <div class="financial-profit-title">
         <span class="iconfont iconchongzhi"></span>
-        <div class="financial-profit-titles">订单统计</div>  
+        <div class="financial-profit-titles">订单统计</div> 
+        <div class="financial-profit-titles">盈利统计</div>
+        <span>今日</span>
+        <span>近月</span>
+        <span>今年</span>
+        <el-date-picker
+            v-model="financialTime"
+            clearable
+            type="daterange"
+            value-format="yyyy-MM-dd"
+            range-separator="至"
+            start-placeholder="开始日期"
+            end-placeholder="结束日期"
+            :picker-options="pickerOptions">
+        </el-date-picker> 
     </div>
     <div class="financial-profit-chart" ref="financialOrderEcharts">     
     </div>
