5fb37175c4268d8db7d8db1d0d0f4e8b28bbabe6
Mr.Y <1324901147@qq.com>
2020-02-14 21:15:43 +0800

    腾讯云设置：对象存储进行中

---
+++ resources/frame/src/admin/view/site/global/tencentCloudConfig/tencentCloudConfigCosView.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+      <Card header="对象存储配置"></Card>
+      <Card header="名称：">
+        <CardRow description="">
+          <el-input v-model="cosName" clearable></el-input>
+        </CardRow>
+      </Card>
+      <Card header="地域：">
+        <CardRow description="">
+          <el-input v-model="cosArea" clearable></el-input>
+        </CardRow>
+      </Card>
+      <Card header="数据万象处理域名：">
+        <CardRow description="">
+          <el-input v-model="cosDomainName" clearable></el-input>
+        </CardRow>
+      </Card>
+      <Card class="footer-btn">
+        <el-button type="primary" size="medium" @click="submission">提交</el-button>
+      </Card>
+    </div>
+</template>
+
+<script>
+  import tencentCloudConfigCosCon from '../../../../controllers/site/global/tencentCloundSet/tencentCloudConfigCosCon'
+  export default {
+      name: "tencentCloudConfigCosView",
+    ...tencentCloudConfigCosCon
+  }
+</script>
+
