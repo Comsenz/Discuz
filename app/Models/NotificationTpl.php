d23ea389f547af1a558e4f790868225574d26df3
yulei745 <yleimm@gmail.com>
2020-02-17 14:28:37 +0800

    fixed system fixed

--- app/Models/NotificationTpl.php
+++ app/Models/NotificationTpl.php
@@ -8,5 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class NotificationTpl extends Model
 {
+    const OPEN = 1;
+
     public $timestamps = false;
 }
--- app/Notifications/System.php
+++ app/Notifications/System.php
@@ -7,7 +7,12 @@
 
 namespace App\Notifications;
 
+use App\MessageTemplate\StatusMessage;
+use App\Models\NotificationTpl;
+use App\Models\User;
+use Discuz\Contracts\Setting\SettingsRepository;
 use Illuminate\Notifications\Notification;
+use Illuminate\Support\Arr;
 
 class System extends Notification
 {
@@ -15,21 +20,62 @@ class System extends Notification
 
     protected $type;
 
+    protected $tplData;
+
+    protected $message;
+
+    protected $settings;
+
+    protected $tplIds = [
+        'ban' => 10,
+        'normal' => 11,
+        'mod_normal' => 2,
+        'mod' => 3
+    ];
+
     public function __construct($type, $data = [])
     {
         $this->type = $type;
         $this->data = $data;
+
+        $this->message = app()->make($type);
+        $this->settings = app()->make(SettingsRepository::class);
     }
 
     public function via($notifiable)
     {
-        return ['database'];
+        $tplId = $this->message->getTplId();
+        if($this->message instanceof StatusMessage) {
+            $actionType = User::enumStatus($notifiable->status);
+            if($this->isMod()) {
+                $notifiable->status == 0 && $actionType = 'mod_'.$actionType;
+            }
+            $tplId = Arr::get($this->tplIds, $actionType);
+        }
+
+        $this->getTplData($tplId);
+
+        $this->message->setTplData($this->tplData);
+
+        //开启状态发送系统消息
+        if(!is_null($this->tplData) && $this->tplData->status == NotificationTpl::OPEN) {
+            return ['database'];
+        }
+        return [];
     }
 
     public function toDatabase($notifiable)
     {
-        $message = app()->make($this->type);
+        return $this->message->notifiable($notifiable)->template($this->data);
+    }
 
-        return $message->notifiable($notifiable)->template($this->data);
+    protected function getTplData($id)
+    {
+        return $this->tplData ? $this->tplData : $this->tplData = NotificationTpl::find($id);
+    }
+
+    protected function isMod()
+    {
+        return (bool)$this->settings->get('register_validate');
     }
 }
