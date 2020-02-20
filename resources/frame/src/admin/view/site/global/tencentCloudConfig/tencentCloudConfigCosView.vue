6a40a8c6b8a68e7a708a487e9fcf4e87ef3445bb
huangxiaomeng <huangxiaomeng@163.com>
2020-02-20 18:22:18 +0800

    修改文案信息

--- resources/frame/src/admin/view/site/global/tencentCloudConfig/tencentCloudConfigCosView.vue
+++ resources/frame/src/admin/view/site/global/tencentCloudConfig/tencentCloudConfigCosView.vue
@@ -2,17 +2,17 @@
     <div>
       <Card header="对象存储配置"></Card>
       <Card header="名称：">
-        <CardRow description="">
+        <CardRow description="填写存储桶基本配置中的空间名称">
           <el-input v-model="cosName" clearable></el-input>
         </CardRow>
       </Card>
       <Card header="地域：">
-        <CardRow description="">
+        <CardRow description="填写存储桶基本配置中的所属地域，例如：ap-beijing">
           <el-input v-model="cosArea" clearable></el-input>
         </CardRow>
       </Card>
       <Card header="数据万象处理域名：">
-        <CardRow description="">
+        <CardRow description="填写存储桶基本配置中的访问域名">
           <el-input v-model="cosDomainName" clearable></el-input>
         </CardRow>
       </Card>
