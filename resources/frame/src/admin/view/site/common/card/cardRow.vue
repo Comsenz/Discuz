df78db1205907c04343568eed26780410c83a479
Mr.Y <1324901147@qq.com>
2020-02-21 15:34:23 +0800

    后台：公共组件card组件，添加新功能：支持多行换行

--- resources/frame/src/admin/view/site/common/card/cardRow.vue
+++ resources/frame/src/admin/view/site/common/card/cardRow.vue
@@ -4,7 +4,8 @@
         <slot></slot>
       </div>
       <div class="card-row-rf">
-        <span>{{$attrs.description}}</span>
+        <span v-if="!row">{{$attrs.description}}</span>
+        <span v-else v-html="fixText($attrs.description)"></span>
         <slot name="tail"></slot>
       </div>
 
@@ -14,7 +15,25 @@
 <script>
 import '../../../../scss/site/module/common/card/cardRow.scss';
 export default {
-    name: "form-row"
+  name: "form-row",
+  data:function () {
+    return {
+
+    }
+  },
+  props:{
+    row:{
+      type:Boolean,  //传入row开启换行，需要在换行的地方添加<br/>
+      default:false
+    }
+  },
+  methods:{
+    fixText (text) {
+      let replaceRegex = /(\n\r|\r\n|\r|\n)/g;
+      text = text || '';
+      return text.replace(replaceRegex, "<br/>");
+    }
+  }
 }
 </script>
 
