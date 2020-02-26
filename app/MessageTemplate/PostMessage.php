5223a9726476d8e0d48922b678578470ccd36200
Harvey <619596123@qq.com>
2020-02-26 21:52:29 +0800

    修改POST和UserStatus通知

--- app/MessageTemplate/PostMessage.php
+++ app/MessageTemplate/PostMessage.php
@@ -7,6 +7,12 @@
 
 namespace App\MessageTemplate;
 
+/**
+ * 内容修改通知
+ *
+ * Class PostMessage
+ * @package App\MessageTemplate
+ */
 class PostMessage extends BasePostMessage
 {
     protected $tplId = 9;
