45fdb7e949859666985840fdfc12728d2e98c8d9
gudanxin1314 <56377852+gudanxin1314@users.noreply.github.com>
2020-03-01 14:33:52 +0800

    从详情页未登录时也可以进入用户主页

--- resources/frame/src/template/default/controllers/m_site/circle/details/longTextDetailsCon.js
+++ resources/frame/src/template/default/controllers/m_site/circle/details/longTextDetailsCon.js
@@ -66,14 +66,14 @@ export default {
     },
     //点击用户名称，跳转到用户主页
     jumpPerDet:function(id){
-      if(!this.userId){
-        this.$router.push({
-          path:'/login-user',
-          name:'login-user'
-        })
-      } else {
-        this.$router.push({ path:'/home-page'+'/'+id});
-      }
+    //   if(!this.userId){
+    //     this.$router.push({
+    //       path:'/login-user',
+    //       name:'login-user'
+    //     })
+    //   } else {
+      this.$router.push({ path:'/home-page'+'/'+id});
+      // }
     },
     //初始化请求用户信息
     loadUserInfo(){
