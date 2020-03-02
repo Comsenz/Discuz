fb56f2444a25dfb0550fc445bd605d8ed4bf0721
Mr.Y <1324901147@qq.com>
2020-03-02 22:04:45 +0800

    修改bug：验证密码错误时，清空密码

--- resources/frame/src/template/default/controllers/m_site/myInfo/paymentPasswordSet/verifyPayPasswordCon.js
+++ resources/frame/src/template/default/controllers/m_site/myInfo/paymentPasswordSet/verifyPayPasswordCon.js
@@ -38,6 +38,7 @@ export default {
       }).then(res=>{
         console.log(res);
         if (res.errors){
+          this.value = '';
           if (res.errors[0].detail){
             this.$toast.fail(res.errors[0].code + '\n' + res.errors[0].detail[0])
           } else {
