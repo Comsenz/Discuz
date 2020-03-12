12afcf21d5f39ba3e089b88649d31277b5e0d4ad
gyn <920791506@qq.com>
2020-03-12 14:57:38 +0800

    修改体现

--- resources/frame/src/template/default/controllers/m_site/login/wxLoginBdCon.js
+++ resources/frame/src/template/default/controllers/m_site/login/wxLoginBdCon.js
@@ -8,24 +8,24 @@ import webDb from '../../../../../helpers/webDbHelper';
 import appCommonH from "../../../../../helpers/commonHelper";
 
 export default {
-  data:function () {
+  data: function () {
     return {
-      userName:'',    //用户名
-      password:'',    //密码
-      siteMode:'',    //站点信息
-      openid:'',       //微信openid
-      wxurl:'',
-      platform:''
+      userName: '',    //用户名
+      password: '',    //密码
+      siteMode: '',    //站点信息
+      openid: '',       //微信openid
+      wxurl: '',
+      platform: ''
     }
   },
 
-  components:{
+  components: {
     LoginHeader,
     LoginFooter
   },
 
-  methods:{
-    loginBdClick(){
+  methods: {
+    loginBdClick() {
       this.appFetch({
         url: "login",
         method: "post",
@@ -34,15 +34,15 @@ export default {
             "attributes": {
               username: this.userName,
               password: this.password,
-              openid:this.openid,
-              platform:this.platform
+              openid: this.openid,
+              platform: this.platform
             },
           }
         }
       }).then(res => {
 
-        if (res.errors){
-          if (res.errors[0].detail){
+        if (res.errors) {
+          if (res.errors[0].detail) {
             this.$toast.fail(res.errors[0].code + '\n' + res.errors[0].detail[0])
           } else {
             this.$toast.fail(res.errors[0].code);
@@ -54,22 +54,22 @@ export default {
           webDb.setLItem('Authorization', token);
           webDb.setLItem('tokenId', tokenId);
           let beforeVisiting = webDb.getSItem('beforeVisiting');
-          this.$router.push({path:webDb.getSItem('beforeVisiting')});
+          this.$router.push({ path: webDb.getSItem('beforeVisiting') });
 
-          this.getUsers(tokenId).then(res=>{
-            if (res.readdata._data.paid){
+          this.getUsers(tokenId).then(res => {
+            if (res.readdata._data.paid) {
               if (beforeVisiting) {
-                this.$router.replace({path: beforeVisiting});
-                webDb.setSItem('beforeState',1);
+                this.$router.replace({ path: beforeVisiting });
+                webDb.setSItem('beforeState', 1);
               } else {
-                this.$router.push({path: '/'});
+                this.$router.push({ path: '/' });
               }
             } else {
               webDb.setLItem('foregroundUser', res.data.attributes.username);
-              if (this.siteMode === 'pay'){
-                this.$router.push({path:'pay-circle-login'});
-              } else if (this.siteMode === 'public'){
-                this.$router.push({path:'/'});
+              if (this.siteMode === 'pay') {
+                this.$router.push({ path: 'pay-circle-login' });
+              } else if (this.siteMode === 'public') {
+                this.$router.push({ path: '/' });
               } else {
                 //缺少参数，请刷新页面
               }
@@ -86,32 +86,32 @@ export default {
     /*
     * 接口请求
     * */
-    getWatchHref(code,state,sessionId){
+    getWatchHref(code, state, sessionId) {
       this.appFetch({
-        url:'wechat',
-        method:'get',
-        data:{
-          code:code,
-          state:state,
-          sessionId:sessionId,
+        url: 'wechat',
+        method: 'get',
+        data: {
+          code: code,
+          state: state,
+          sessionId: sessionId,
         }
-      }).then(res=>{
-        if (res.errors){
+      }).then(res => {
+        if (res.errors) {
 
           let wxStatus = res.errors[0].status;
           let openid = res.errors[0].user.openid;
 
-          if (wxStatus == 400){
+          if (wxStatus == 400) {
             //微信跳转
             this.openid = openid;
-            webDb.setLItem('openid',openid);
-            this.$router.push({path: '/wx-login-bd'});
+            webDb.setLItem('openid', openid);
+            this.$router.push({ path: '/wx-login-bd' });
           }
         } else if (res.data.attributes.location) {
           //获取地址
           this.wxurl = res.data.attributes.location;
           window.location.href = res.data.attributes.location
-        } else if (res.data.attributes.access_token){
+        } else if (res.data.attributes.access_token) {
 
           this.$toast.success('登录成功');
           let token = res.data.attributes.access_token;
@@ -121,73 +121,73 @@ export default {
           let beforeVisiting = webDb.getSItem('beforeVisiting');
 
           if (beforeVisiting) {
-            this.$router.replace({path: beforeVisiting});
-            webDb.setSItem('beforeState',1);
+            this.$router.replace({ path: beforeVisiting });
+            webDb.setSItem('beforeState', 1);
           } else {
-            this.$router.push({path: '/'});
+            this.$router.push({ path: '/' });
           }
 
         } else {
           //任何情况都不符合
         }
-      }).catch(err=>{
+      }).catch(err => {
       })
     },
-    getForum(){
+    getForum() {
       this.appFetch({
-        url:'forum',
-        method:'get',
-        data:{}
-      }).then(res=>{
+        url: 'forum',
+        method: 'get',
+        data: {}
+      }).then(res => {
         this.siteMode = res.readdata._data.set_site.site_mode;
-        webDb.setLItem('siteInfo',res.readdata);
-      }).catch(err=>{
+        webDb.setLItem('siteInfo', res.readdata);
+      }).catch(err => {
       })
     },
-    getUsers(id){
+    getUsers(id) {
       return this.appFetch({
-        url:'users',
-        method:'get',
-        splice:'/' + id,
-        headers:{'Authorization': 'Bearer ' + webDb.getLItem('Authorization')},
-        data:{
-          include:['groups']
+        url: 'users',
+        method: 'get',
+        splice: '/' + id,
+        headers: { 'Authorization': 'Bearer ' + webDb.getLItem('Authorization') },
+        data: {
+          include: ['groups']
         }
-      }).then(res=>{
-        if(res.errors){
-            this.$toast.fail(res.errors[0].code);
-        }else {
+      }).then(res => {
+        if (res.errors) {
+          this.$toast.fail(res.errors[0].code);
+        } else {
           return res;
         }
-      }).catch(err=>{
+      }).catch(err => {
       })
     },
-    getWatchHrefPC(code,state,sessionId){
+    getWatchHrefPC(code, state, sessionId) {
       this.appFetch({
-        url:'wxLogin',
-        method:'get',
-        data:{
-          code:code,
-          state:state,
-          sessionId:sessionId,
+        url: 'wxLogin',
+        method: 'get',
+        data: {
+          code: code,
+          state: state,
+          sessionId: sessionId,
         }
-      }).then(res=>{
-        if (res.errors){
+      }).then(res => {
+        if (res.errors) {
 
           let wxStatus = res.errors[0].status;
           let openid = res.errors[0].user.openid;
 
-          if (wxStatus == 400){
+          if (wxStatus == 400) {
             //微信跳转
             this.openid = openid;
-            webDb.setLItem('openid',openid);
-            this.$router.push({path: '/wx-login-bd'});
+            webDb.setLItem('openid', openid);
+            this.$router.push({ path: '/wx-sign-up-bd' });
           }
         } else if (res.data.attributes.location) {
           //获取地址
           this.wxurl = res.data.attributes.location;
           window.location.href = res.data.attributes.location;
-        } else if (res.data.attributes.access_token){
+        } else if (res.data.attributes.access_token) {
 
           this.$toast.success('登录成功');
           let token = res.data.attributes.access_token;
@@ -197,21 +197,21 @@ export default {
           let beforeVisiting = webDb.getSItem('beforeVisiting');
 
           if (beforeVisiting) {
-            this.$router.replace({path: beforeVisiting});
-            webDb.setSItem('beforeState',1);
+            this.$router.replace({ path: beforeVisiting });
+            webDb.setSItem('beforeState', 1);
           } else {
-            this.$router.push({path: '/'});
+            this.$router.push({ path: '/' });
           }
 
         } else {
           //任何情况都不符合
         }
-      }).catch(err=>{
+      }).catch(err => {
       })
     }
 
   },
-  created(){
+  created() {
     let code = this.$router.history.current.query.code;
     let state = this.$router.history.current.query.state;
     let sessionId = this.$router.history.current.query.sessionId;
@@ -223,16 +223,16 @@ export default {
     // webDb.setLItem('code',code);
     // webDb.setLItem('state',state);
 
-    if (isWeixin){
+    if (isWeixin) {
       this.platform = 'mp';
-      if (!code && !state){
+      if (!code && !state) {
         // this.getWatchHref()
       } else {
         // this.getWatchHref(code,state,sessionId);
       }
-    }else {
+    } else {
       this.platform = 'dev';
-      if (this.openid === ''){
+      if (this.openid === '') {
         //PC端：没有openid
         // this.getWatchHrefPC(code,state,sessionId);
       }
