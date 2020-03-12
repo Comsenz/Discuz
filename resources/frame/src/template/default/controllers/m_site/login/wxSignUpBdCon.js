792b06238fc511e2cdcdb9effaa3ac7dc2caca99
YangPengPc <1324901147@qq.com>
2020-03-12 16:07:23 +0800

    修改微信

--- resources/frame/src/template/default/controllers/m_site/login/wxSignUpBdCon.js
+++ resources/frame/src/template/default/controllers/m_site/login/wxSignUpBdCon.js
@@ -249,8 +249,8 @@ export default {
     let code = this.$router.history.current.query.code;
     let state = this.$router.history.current.query.state;
     let sessionId = this.$router.history.current.query.sessionId;
-    let qcloud_captcha = browserDb.getLItem('siteInfo')._data.qcloud.qcloud_captcha;
-    let register_captcha = browserDb.getLItem('siteInfo')._data.set_reg.register_captcha;
+    let qcloud_captcha = webDb.getLItem('siteInfo')._data.qcloud.qcloud_captcha;
+    let register_captcha = webDb.getLItem('siteInfo')._data.set_reg.register_captcha;
     if (qcloud_captcha && register_captcha) {
       this.signUpBdClickShow = false
     }
