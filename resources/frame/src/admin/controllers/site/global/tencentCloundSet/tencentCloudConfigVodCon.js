101e4bf764046d08d798da4d9883d5de5e0bb07c
gyn <920791506@qq.com>
2020-03-12 16:56:39 +0800

    腾讯云设置视频设置

--- resources/frame/src/admin/controllers/site/global/tencentCloundSet/tencentCloudConfigVodCon.js
+++ resources/frame/src/admin/controllers/site/global/tencentCloundSet/tencentCloudConfigVodCon.js
@@ -2,75 +2,79 @@ import Card from '../../../../view/site/common/card/card';
 import CardRow from '../../../../view/site/common/card/cardRow';
 
 export default {
-  data:function () {
+  data: function () {
     return {
-      vodTranscode:'',
-      vodExt:'',
-      vodSize:'',//短信签名
+      vodTranscode: '', //转码模板
+      vodExt: '',
+      vodSize: '',//短信签名
 
     }
   },
-  created(){
+  created() {
     var type = this.$route.query.type;
     this.type = type;
     this.tencentCloudSms()
   },
-  methods:{
-    tencentCloudSms(){
+  methods: {
+    tencentCloudSms() {
       this.appFetch({
-        url:'forum',
-        method:'get',
-        data:{}
-      }).then(res=>{
-        if (res.errors){
+        url: 'forum',
+        method: 'get',
+        data: {}
+      }).then(res => {
+        if (res.errors) {
           this.$message.error(res.errors[0].code);
-        }else {
-          this.vodTranscode = res.readdata._data.qcloud_vod_transcode;
+        } else {
+          this.vodTranscode = res.readdata._data.qcloud.qcloud_vod_transcode;
           this.vodExt = res.readdata._data.qcloud.qcloud_vod_ext;
           this.vodSize = res.readdata._data.qcloud.qcloud_vod_size;
         }
       })
     },
-    Submission(){
+    Submission() {
+      if (this.vodTranscode === '') {
+        this.$message("请填写转码模板");
+        return
+      }
       this.appFetch({
-        url:'settings',
-        method:'post',
-        data:{
-          "data":[
+        url: 'settings',
+        method: 'post',
+        data: {
+          "data": [
             {
-              "attributes":{
-                "key":'qcloud_vod_transcode',
-                "value":this.vodTranscode,
+              "attributes": {
+                "key": 'qcloud_vod_transcode',
+                "value": this.vodTranscode,
                 "tag": "qcloud"
               }
             },
             {
-              "attributes":{
-                "key":'qcloud_vod_ext',
-                "value":this.vodExt,
+              "attributes": {
+                "key": 'qcloud_vod_ext',
+                "value": this.vodExt,
                 "tag": "qcloud",
               }
-              },
-              {
-                "attributes":{
-                  "key":'qcloud_vod_size',
-                  "value":this.vodSize,
-                  "tag": "qcloud",
-                }
+            },
+            {
+              "attributes": {
+                "key": 'qcloud_vod_size',
+                "value": this.vodSize,
+                "tag": "qcloud",
               }
+            }
           ]
         }
-      }).then(res=>{
-        if (res.errors){
+      }).then(res => {
+        if (res.errors) {
           this.$message.error(res.errors[0].code);
-        }else {
-          this.$message({message: '提交成功', type: 'success'});
+        } else {
+          this.$message({ message: '提交成功', type: 'success' });
         }
       })
     }
 
   },
-  components:{
+  components: {
     Card,
     CardRow
   }
