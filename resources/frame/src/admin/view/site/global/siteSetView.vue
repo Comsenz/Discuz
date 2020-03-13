076c37f91ef7fff8d046e5b8ae6e293f907b6528
gyn <920791506@qq.com>
2020-03-13 14:20:29 +0800

    站点隐藏及首页

--- resources/frame/src/admin/view/site/global/siteSetView.vue
+++ resources/frame/src/admin/view/site/global/siteSetView.vue
@@ -1,26 +1,25 @@
 <template>
-    <div class="site-set-box">
-      <Card header="站点名称：">
-        <CardRow description="你的Discuz!Q 站点的名称">
-            <el-input placeholder="站点名称" v-model="siteName"></el-input>
-        </CardRow>
-      </Card>
+  <div class="site-set-box">
+    <Card header="站点名称：">
+      <CardRow description="你的Discuz!Q 站点的名称">
+        <el-input placeholder="站点名称" v-model="siteName"></el-input>
+      </CardRow>
+    </Card>
 
-      <Card header="站点介绍：">
-        <CardRow description="你的Discuz!Q 站点的介绍">
-            <el-input
-              type="textarea"
-              :autosize="{ minRows: 4, maxRows: 4}"
-              placeholder="站点介绍"
-              v-model="siteIntroduction"
-              >
-            </el-input>
-        </CardRow>
-      </Card>
+    <Card header="站点介绍：">
+      <CardRow description="你的Discuz!Q 站点的介绍">
+        <el-input
+          type="textarea"
+          :autosize="{ minRows: 4, maxRows: 4}"
+          placeholder="站点介绍"
+          v-model="siteIntroduction"
+        ></el-input>
+      </CardRow>
+    </Card>
 
-      <Card header="站点LOGO：">
-        <CardRow description="你的Discuz!Q 站点的LOGO">
-          <!-- <el-upload
+    <Card header="站点LOGO：">
+      <CardRow description="你的Discuz!Q 站点的LOGO">
+        <!-- <el-upload
             class="avatar-uploader"
             action="#"
             :limit="1"
@@ -30,142 +29,148 @@
             :file-list="siteLogoFile">
             <img v-if="imageUrl" :src="imageUrl" class="avatar">
             <i v-else class="el-icon-plus avatar-uploader-icon"></i>
-          </el-upload> -->
-         <!-- <el-upload
+        </el-upload>-->
+        <!-- <el-upload
             action="https://jsonplaceholder.typicode.com/posts/"
             list-type="picture-card"
             :limit="1"
             :on-preview="handlePictureCardPreview"
             :on-remove="handleRemove">
             <i class="el-icon-plus"></i>
-          </el-upload> -->
-          <!-- <el-dialog :visible.sync="dialogVisible" size="tiny">
+        </el-upload>-->
+        <!-- <el-dialog :visible.sync="dialogVisible" size="tiny">
             <img width="100%" :src="dialogImageUrl" alt="">
-          </el-dialog> -->
-          <!-- <el-button type="text">删除</el-button> -->
+        </el-dialog>-->
+        <!-- <el-button type="text">删除</el-button> -->
 
-          <el-upload
-           class="avatar-uploader"
-            action=""
-            :http-request="uploaderLogo"
-            :show-file-list="false"
-            :on-success="handleAvatarSuccess"
-            @change="handleFile"
-            :before-upload="beforeAvatarUpload"
-            >
-            <div v-if="imageUrl" class="avatar">
-              <img :src="imageUrl" class="avatar-LogoImage" :style="{'width': imgWidht + 'px', height: imgHeight+'px'}">
-            </div>
-           <i v-else class="el-icon-plus avatar-uploader-icon"></i>
-          </el-upload>
-           <el-button type="text" @click="deleteImage">删除</el-button>
-          <!-- <el-dialog :visible.sync="dialogVisible" size="tiny">
+        <el-upload
+          class="avatar-uploader"
+          action
+          :http-request="uploaderLogo"
+          :show-file-list="false"
+          :on-success="handleAvatarSuccess"
+          @change="handleFile"
+          :before-upload="beforeAvatarUpload"
+        >
+          <div v-if="imageUrl" class="avatar">
+            <img
+              :src="imageUrl"
+              class="avatar-LogoImage"
+              :style="{'width': imgWidht + 'px', height: imgHeight+'px'}"
+            />
+          </div>
+          <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+        </el-upload>
+        <el-button type="text" @click="deleteImage">删除</el-button>
+        <!-- <el-dialog :visible.sync="dialogVisible" size="tiny">
             <img width="100%" :src="dialogImageUrl" alt="">
-          </el-dialog> -->
-        </CardRow>
-      </Card>
-       <Card header="站长：">
-         <CardRow description="填写站长的用户id">
-             <el-input placeholder="站长" type="number" v-model="siteMasterId"></el-input>
-         </CardRow>
-       </Card>
-      <Card header="站点模式：">
-        <CardRow description="你的Discuz!Q 站点的运行模式">
-          <el-radio @change="radioChange('public')" v-model="radio" label="1">公开模式</el-radio>
-          <el-radio @change="radioChange('pay')" v-model="radio" label="2">付费模式</el-radio>
-        </CardRow>
-      </Card>
+        </el-dialog>-->
+      </CardRow>
+    </Card>
+    <Card header="站长：" v-show="false">
+      <CardRow description="填写站长的用户id">
+        <el-input placeholder="站长" type="number" v-model="siteMasterId"></el-input>
+      </CardRow>
+    </Card>
+    <Card header="站点模式：">
+      <CardRow description="你的Discuz!Q 站点的运行模式">
+        <el-radio @change="radioChange('public')" v-model="radio" label="1">公开模式</el-radio>
+        <el-radio @change="radioChange('pay')" v-model="radio" label="2">付费模式</el-radio>
+      </CardRow>
+    </Card>
 
-      <el-collapse-transition>
-        <div v-show="radio === '2'">
-          <Card  header="加入价格（元）：">
-            <CardRow description="付费模式下，付费成为站点默认角色，需支付的金额">
-              <el-input placeholder="加入价格" type="number" v-model="sitePrice"></el-input>
-            </CardRow>
-          </Card>
+    <el-collapse-transition>
+      <div v-show="radio === '2'">
+        <Card header="加入价格（元）：">
+          <CardRow description="付费模式下，付费成为站点默认角色，需支付的金额">
+            <el-input placeholder="加入价格" type="number" v-model="sitePrice"></el-input>
+          </CardRow>
+        </Card>
 
-          <Card  header="到期时间：">
-            <CardRow description="付费模式下，付费成为站点默认角色，可维持的时间，不填或为0时不限制">
-               加入起
-                  <el-input
-                    style="height: 36PX;width: 80PX"
-                    clearable
-                    placeholder="天数"
-                    type="number"
-                    v-model="siteExpire"
-                    >
-                  </el-input>
-                  天后
+        <Card header="到期时间：">
+          <CardRow description="付费模式下，付费成为站点默认角色，可维持的时间，不填或为0时不限制">
+            加入起
+            <el-input
+              style="height: 36PX;width: 80PX"
+              clearable
+              placeholder="天数"
+              type="number"
+              v-model="siteExpire"
+            ></el-input>
+天后
+          </CardRow>
+        </Card>
+      </div>
+    </el-collapse-transition>
 
-            </CardRow>
-          </Card>
-
-        </div>
-
-      </el-collapse-transition>
-
-      <Card  header="主题打赏金额分成比例：">
-        <CardRow description="主题打赏的分成比例设置，两者加起来必须为10，不填时默认为作者10、平台0">
-          <div class="proportion-box">
-            <span>作者</span>
-            <el-input class="" size="small" type="number" v-model="siteAuthorScale" @blur.native.capture="onblurFun"></el-input>
-          </div>
-          <div class="proportion-box">
-            <span>平台(站长)</span>
-            <el-input size="small" type="number" v-model="siteMasterScale" @blur.native.capture="onblurFun"></el-input>
-          </div>
-        </CardRow>
-      </Card>
-
-      <Card header="网站备案信息：">
-        <CardRow description="你的Discuz!Q 站点的 ICP 备案编号">
-          <el-input v-model='siteRecord'></el-input>
-        </CardRow>
-      </Card>
-
-      <Card header="第三方统计：">
-        <CardRow description="你的Discuz!Q 网站的第三方统计代码">
+    <Card header="主题打赏金额分成比例：">
+      <CardRow description="主题打赏的分成比例设置，两者加起来必须为10，不填时默认为作者10、平台0">
+        <div class="proportion-box">
+          <span>作者</span>
           <el-input
-            type="textarea"
-            :autosize="{ minRows: 4, maxRows: 4}"
-              v-model='siteStat'
-            >
-          </el-input>
-        </CardRow>
-      </Card>
-
-      <Card header="关闭站点：">
-        <CardRow description="暂时将网站关闭，其他人无法访问，但不影响管理员访问">
-          <el-radio @change="radioChangeClose('1')" v-model="radio2" label="1">是</el-radio>
-          <el-radio @change="radioChangeClose('2')" v-model="radio2" label="2">否</el-radio>
-        </CardRow>
-      </Card>
-
-      <el-collapse-transition>
-        <div v-show="radio2 === '1'">
-          <Card  header="">
-            <CardRow description="站点关闭时出现的提示信息">
-              <el-input v-model="siteCloseMsg"></el-input>
-            </CardRow>
-          </Card>
+            class
+            size="small"
+            type="number"
+            v-model="siteAuthorScale"
+            @blur.native.capture="onblurFun"
+          ></el-input>
         </div>
-      </el-collapse-transition>
+        <div class="proportion-box">
+          <span>平台(站长)</span>
+          <el-input
+            size="small"
+            type="number"
+            v-model="siteMasterScale"
+            @blur.native.capture="onblurFun"
+          ></el-input>
+        </div>
+      </CardRow>
+    </Card>
 
-      <Card class="footer-btn">
-        <el-button type="primary" size="medium" @click="siteSetPost">提交</el-button>
-      </Card>
-    </div>
+    <Card header="网站备案信息：">
+      <CardRow description="你的Discuz!Q 站点的 ICP 备案编号">
+        <el-input v-model="siteRecord"></el-input>
+      </CardRow>
+    </Card>
+
+    <Card header="第三方统计：">
+      <CardRow description="你的Discuz!Q 网站的第三方统计代码">
+        <el-input type="textarea" :autosize="{ minRows: 4, maxRows: 4}" v-model="siteStat"></el-input>
+      </CardRow>
+    </Card>
+
+    <Card header="关闭站点：">
+      <CardRow description="暂时将网站关闭，其他人无法访问，但不影响管理员访问">
+        <el-radio @change="radioChangeClose('1')" v-model="radio2" label="1">是</el-radio>
+        <el-radio @change="radioChangeClose('2')" v-model="radio2" label="2">否</el-radio>
+      </CardRow>
+    </Card>
+
+    <el-collapse-transition>
+      <div v-show="radio2 === '1'">
+        <Card header>
+          <CardRow description="站点关闭时出现的提示信息">
+            <el-input v-model="siteCloseMsg"></el-input>
+          </CardRow>
+        </Card>
+      </div>
+    </el-collapse-transition>
+
+    <Card class="footer-btn">
+      <el-button type="primary" size="medium" @click="siteSetPost">提交</el-button>
+    </Card>
+  </div>
 </template>
 <style>
-  .disabled .el-upload--picture-card {
-      display: none;
-  }
+.disabled .el-upload--picture-card {
+  display: none;
+}
 </style>
 <script>
-import '../../../scss/site/module/globalStyle.scss';
-import siteSetCon from '../../../controllers/site/global/siteSetCon';
+import "../../../scss/site/module/globalStyle.scss";
+import siteSetCon from "../../../controllers/site/global/siteSetCon";
 export default {
-    name: "site-set-view",
+  name: "site-set-view",
   ...siteSetCon
-}
+};
 </script>
--- resources/frame/src/template/default/view/m_site/common/headerView.vue
+++ resources/frame/src/template/default/view/m_site/common/headerView.vue
@@ -5,60 +5,84 @@
       v-model="popupShow"
       position="right"
       :style="{'height':'100%','right': (!isPhone && !isWeixin) ? (viewportWidth - 640)/2+'px' : '0'}"
-   >
-    <sidebar :isPayVal = "isPayVal"></sidebar>
+    >
+      <sidebar :isPayVal="isPayVal"></sidebar>
     </van-popup>
     <!-- 侧边栏 -E -->
     <div class="headerBox" v-if="$route.meta.oneHeader">
       <div class="invitePerDet aaa" v-show="invitePerDet">
         <!-- <div class="invitePerDet" v-show="invitePerDet"> -->
         <!-- <img src="../../../../../../static/images/noavatar.gif" class="inviteHead"> -->
-          <img v-if="userInfoAvatarUrl" :src="userInfoAvatarUrl" alt="" class="inviteHead">
-          <img v-else="" :src="appConfig.staticBaseUrl+'/images/noavatar.gif'" alt="" class="inviteHead">
-          <div class="inviteName" v-if="invitePerDet && userInfoName" v-model="userInfoName">{{userInfoName}}</div>
-          <!-- <div class="inviteName" v-else="">该用户已被删除</div> -->
-          <p class="inviteWo" v-show="invitationShow">邀请您加入</p>
-          <div class="followBox" v-if="followShow && followDet">
-            <span>关注：{{followDet._data.followCount}}</span>
-            <span>被关注：{{followDet._data.fansCount}}</span>
-            <div href="javascript:;" v-if="!equalId" class="followStatus">
-              <a href="javascript:;" class="" v-if="intiFollowVal == '0'" @click="followCli(intiFollowVal)">关注TA</a>
-              <a href="javascript:;" class="" v-else-if="intiFollowVal == '2'" @click="followCli(intiFollowVal)">相互关注</a>
-              <a href="javascript:;" class="" v-else-if="intiFollowVal == '1'" @click="followCli(intiFollowVal)">已关注</a>
-              <!-- <a href="javascript:;" class="" v-else="" style="display: none;"></a> -->
-            </div>
-
+        <img v-if="userInfoAvatarUrl" :src="userInfoAvatarUrl" alt class="inviteHead" />
+        <img v-else :src="appConfig.staticBaseUrl+'/images/noavatar.gif'" alt class="inviteHead" />
+        <div
+          class="inviteName"
+          v-if="invitePerDet && userInfoName"
+          v-model="userInfoName"
+        >{{userInfoName}}</div>
+        <!-- <div class="inviteName" v-else="">该用户已被删除</div> -->
+        <p class="inviteWo" v-show="invitationShow">邀请您加入</p>
+        <div class="followBox" v-if="followShow && followDet">
+          <span>关注：{{followDet._data.followCount}}</span>
+          <span>被关注：{{followDet._data.fansCount}}</span>
+          <div href="javascript:;" v-if="!equalId" class="followStatus">
+            <a
+              href="javascript:;"
+              class
+              v-if="intiFollowVal == '0'"
+              @click="followCli(intiFollowVal)"
+            >关注TA</a>
+            <a
+              href="javascript:;"
+              class
+              v-else-if="intiFollowVal == '2'"
+              @click="followCli(intiFollowVal)"
+            >相互关注</a>
+            <a
+              href="javascript:;"
+              class
+              v-else-if="intiFollowVal == '1'"
+              @click="followCli(intiFollowVal)"
+            >已关注</a>
+            <!-- <a href="javascript:;" class="" v-else="" style="display: none;"></a> -->
           </div>
+        </div>
       </div>
       <div class="headeGap" v-if="!searchIconShow && !menuIconShow"></div>
       <div class="headOpe" v-if="searchIconShow || menuIconShow">
         <!-- <span class="icon iconfont icon-search" v-show="backIconShow"></span> -->
         <span class="icon iconfont icon-search" @click="searchJump" v-show="searchIconShow"></span>
-        <span class="icon iconfont icon-Shape relative" is-link @click="showPopup" v-show="menuIconShow"><i class="noticeNew" v-if="noticeSum>0"></i></span>
+        <span
+          class="icon iconfont icon-Shape relative"
+          is-link
+          @click="showPopup"
+          v-show="menuIconShow"
+        >
+          <i class="noticeNew" v-if="noticeSum>0"></i>
+        </span>
       </div>
       <div class="logoBox" v-show="logoShow">
-        <img v-if="logo" :src="logo" class="logo">
-        <img v-else="" :src="appConfig.staticBaseUrl+'/images/logo.png'" class="logo">
+        <img v-if="logo" :src="logo" class="logo" />
+        <img v-else :src="appConfig.staticBaseUrl+'/images/logo.png'" class="logo" />
       </div>
-
 
       <div class="circleDet" v-show="perDetShow" v-if="siteInfo">
         <span>主题：{{siteInfo._data.other.count_threads}}</span>
-        <span>成员：{{siteInfo._data.other.count_users}}</span>
-        <span v-if="siteInfo._data.set_site.site_author">站长：{{siteInfo._data.set_site.site_author.username}}</span>
-        <span v-else="">站长：无</span>
+        <span>帖子：{{siteInfo._data.other.count_posts}}</span>
+        <span v-if="siteInfo._data.other.count_users">成员：{{siteInfo._data.other.count_users}}</span>
+        <span v-else>成员：无</span>
       </div>
       <div class="navBox" id="testNavBar" :class="{'fixedNavBar': isfixNav}" v-show="navShow">
         <van-tabs v-model="navActi">
           <van-tab>
-            <span slot="title" class="title-span" v-on:click="categoriesCho(0)">
-                全部
-            </span>
+            <span slot="title" class="title-span" v-on:click="categoriesCho(0)">全部</span>
           </van-tab>
           <van-tab v-for="(cateChi, index) in categories" :key="index">
-            <span slot="title" class="title-span" v-on:click="categoriesCho(cateChi._data.id)">
-                {{cateChi._data.name}}
-            </span>
+            <span
+              slot="title"
+              class="title-span"
+              v-on:click="categoriesCho(cateChi._data.id)"
+            >{{cateChi._data.name}}</span>
           </van-tab>
         </van-tabs>
       </div>
@@ -66,16 +90,15 @@
   </section>
 </template>
 <script>
-import mSiteHeader from '../../../controllers/m_site/common/headerCon';
-import Sidebar from '../../m_site/common/sidebarView';
+import mSiteHeader from "../../../controllers/m_site/common/headerCon";
+import Sidebar from "../../m_site/common/sidebarView";
 // import '../../../scss/m_site/mobileIndex.scss';
-import  '../../../defaultLess/m_site/common/common.less';
+import "../../../defaultLess/m_site/common/common.less";
 export default {
   name: "headerView",
-  components:{
-  	Sidebar
+  components: {
+    Sidebar
   },
   ...mSiteHeader
-}
-
+};
 </script>
