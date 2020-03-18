c8b54aa4b939685a4c8ed08c9491fcb12d4ab0d9
gyn <920791506@qq.com>
2020-03-18 14:15:00 +0800

    更新邀请页

--- resources/frame/src/template/default/controllers/m_site/login/payTheFeeCon.js
+++ resources/frame/src/template/default/controllers/m_site/login/payTheFeeCon.js
@@ -8,44 +8,44 @@ import PayMethod from '../../../view/m_site/common/pay/paymentMethodView';
 import webDb from '../../../../../helpers/webDbHelper';
 
 export default {
-  data:function () {
+  data: function () {
     return {
-      sitePrice:'',      //付费站点价钱
-      siteExpire:'',     //到期时间
-      orderSn:'',        //订单号
-      wxPayHref:'',      //微信支付链接
-      qrcodeShow:false,  //pc端显示二维码
-      codeUrl:"",        //支付url，base64
-      amountNum:'',      //支付价钱
-      payStatus:false,   //支付状态
-      payStatusNum:0,    //支付状态次数
-      authorityList:'',  //权限列表
-      tokenId:'',        //用户ID
-      dialogShow:false,  //微信支付确认弹框
-      groupId:'',        //用户组ID
-      limitList:[],      //用户组权限
-      payList:[
+      sitePrice: '',      //付费站点价钱
+      siteExpire: '',     //到期时间
+      orderSn: '',        //订单号
+      wxPayHref: '',      //微信支付链接
+      qrcodeShow: false,  //pc端显示二维码
+      codeUrl: "",        //支付url，base64
+      amountNum: '',      //支付价钱
+      payStatus: false,   //支付状态
+      payStatusNum: 0,    //支付状态次数
+      authorityList: '',  //权限列表
+      tokenId: '',        //用户ID
+      dialogShow: false,  //微信支付确认弹框
+      groupId: '',        //用户组ID
+      limitList: [],      //用户组权限
+      payList: [
         {
-          name:'钱包',
-          icon:'icon-wallet'
+          name: '钱包',
+          icon: 'icon-wallet'
         }
       ],     //支付方式
-      show:false,        //是否显示支付方式
-      errorInfo:'',      //密码错误提示
-      value:'',          //密码
-      walletBalance:'',  //钱包余额
-      walletStatus:'',    //钱包支付密码状态
+      show: false,        //是否显示支付方式
+      errorInfo: '',      //密码错误提示
+      value: '',          //密码
+      walletBalance: '',  //钱包余额
+      walletStatus: '',    //钱包支付密码状态
       payLoading: false,
     }
   },
 
-  components:{
+  components: {
     PayHeader,
     PayMethod
   },
 
-  methods:{
-    payImmediatelyClick(data){
+  methods: {
+    payImmediatelyClick(data) {
       //data返回选中项
 
       let isWeixin = this.appCommonH.isWeixin().isWeixin;
@@ -53,50 +53,51 @@ export default {
 
       if (data.name === '微信支付') {
         this.show = false;
-        if (isWeixin){
+        if (isWeixin) {
           //微信
-          this.getOrderSn().then(()=>{
-            this.orderPay(12).then((res)=>{
-              if (typeof WeixinJSBridge == "undefined"){
-                if( document.addEventListener ){
+          this.getOrderSn().then(() => {
+            this.orderPay(12).then((res) => {
+              if (typeof WeixinJSBridge == "undefined") {
+                if (document.addEventListener) {
                   document.addEventListener('WeixinJSBridgeReady', this.onBridgeReady(res), false);
-                }else if (document.attachEvent){
+                } else if (document.attachEvent) {
                   document.attachEvent('WeixinJSBridgeReady', this.onBridgeReady(res));
                   document.attachEvent('onWeixinJSBridgeReady', this.onBridgeReady(res));
                 }
-              }else{
+              } else {
                 this.onBridgeReady(res);
               }
             })
           });
-        } else if (isPhone){
+        } else if (isPhone) {
           //手机浏览器
-          this.getOrderSn().then(()=>{
-            this.orderPay(11).then((res)=>{
+          this.getOrderSn().then(() => {
+            this.orderPay(11).then((res) => {
+              console.log(res)
               this.wxPayHref = res.readdata._data.wechat_h5_link;
               window.location.href = this.wxPayHref;
 
-              const payPhone = setInterval(()=>{
-                if (this.payStatus && this.payStatusNum > 10){
+              const payPhone = setInterval(() => {
+                if (this.payStatus && this.payStatusNum > 10) {
                   clearInterval(payPhone);
                 }
                 this.getUsersInfo()
-              },3000)
+              }, 3000)
 
             })
           });
         } else {
           //pc
-          this.getOrderSn().then(()=>{
-            this.orderPay(10).then((res)=>{
+          this.getOrderSn().then(() => {
+            this.orderPay(10).then((res) => {
               this.codeUrl = res.readdata._data.wechat_qrcode;
               this.qrcodeShow = true;
-              const pay = setInterval(()=>{
-                if (this.payStatus && this.payStatusNum > 10){
+              const pay = setInterval(() => {
+                if (this.payStatus && this.payStatusNum > 10) {
                   clearInterval(pay);
                 }
                 this.getUsersInfo()
-              },3000)
+              }, 3000)
             })
           });
         }
@@ -104,52 +105,52 @@ export default {
 
     },
 
-    onInput(key){
+    onInput(key) {
 
       this.value = this.value + key;
 
-      if (this.value.length === 6 ) {
+      if (this.value.length === 6) {
         this.errorInfo = '';
-        this.getOrderSn().then(()=>{
-          this.orderPay(20,this.value).then((res)=>{
-            const pay = setInterval(()=>{
-              if (this.payStatus && this.payStatusNum > 10){
+        this.getOrderSn().then(() => {
+          this.orderPay(20, this.value).then((res) => {
+            const pay = setInterval(() => {
+              if (this.payStatus && this.payStatusNum > 10) {
                 clearInterval(pay);
               }
               this.getUsersInfo()
-            },3000)
+            }, 3000)
           })
         })
       }
     },
     //删除
-    onDelete(){
+    onDelete() {
       this.value = this.value.slice(0, this.value.length - 1);
     },
     //关闭
-    onClose(){
+    onClose() {
       this.value = '';
       this.errorInfo = '';
-      this.payLoading= false;
+      this.payLoading = false;
     },
 
-    leapFrogClick(){
-      this.$router.push({path:'pay-circle-login'})
+    leapFrogClick() {
+      this.$router.push({ path: 'pay-circle-login' })
     },
 
-    onBridgeReady(data){
+    onBridgeReady(data) {
       let that = this;
 
       WeixinJSBridge.invoke(
         'getBrandWCPayRequest', {
-          "appId":data.data.attributes.wechat_js.appId,     //公众号名称，由商户传入
-          "timeStamp":data.data.attributes.wechat_js.timeStamp,         //时间戳，自1970年以来的秒数
-          "nonceStr":data.data.attributes.wechat_js.nonceStr, //随机串
-          "package":data.data.attributes.wechat_js.package,
-          "signType":"MD5",         //微信签名方式：
-          "paySign":data.data.attributes.wechat_js.paySign //微信签名
-        },
-        function(res){
+        "appId": data.data.attributes.wechat_js.appId,     //公众号名称，由商户传入
+        "timeStamp": data.data.attributes.wechat_js.timeStamp,         //时间戳，自1970年以来的秒数
+        "nonceStr": data.data.attributes.wechat_js.nonceStr, //随机串
+        "package": data.data.attributes.wechat_js.package,
+        "signType": "MD5",         //微信签名方式：
+        "paySign": data.data.attributes.wechat_js.paySign //微信签名
+      },
+        function (res) {
           // alert('支付唤醒');
 
           if (res.err_msg == "get_brand_wcpay_request:cancel") {
@@ -176,7 +177,7 @@ export default {
 
         });
 
-      setTimeout(()=>{
+      setTimeout(() => {
         const toast = that.$toast.loading({
           duration: 0, // 持续展示 toast
           forbidClick: true,
@@ -185,18 +186,18 @@ export default {
         let second = 5;
         const timer = setInterval(() => {
           second--;
-          this.getUsers(that.tokenId).then(res=>{
+          this.getUsers(that.tokenId).then(res => {
 
-            if (res.errors){
+            if (res.errors) {
               clearInterval(timer);
               toast.message = '支付失败，请重新支付！';
-              setTimeout(()=>{
+              setTimeout(() => {
                 toast.clear();
-              },2000)
+              }, 2000)
             } else {
-              if (second > 0 || !res.readdata._data.paid){
+              if (second > 0 || !res.readdata._data.paid) {
                 toast.message = `正在查询订单...`;
-              } else if (res.readdata._data.paid){
+              } else if (res.readdata._data.paid) {
                 clearInterval(timer);
                 webDb.setLItem('foregroundUser', res.data.attributes.username);
                 this.show = false;
@@ -207,9 +208,9 @@ export default {
                 let beforeVisiting = webDb.getSItem('beforeVisiting');
 
                 if (beforeVisiting) {
-                  this.$router.push({path: beforeVisiting})
+                  this.$router.push({ path: beforeVisiting })
                 } else {
-                  this.$router.push({path: '/'})
+                  this.$router.push({ path: '/' })
                 }
               } else {
                 clearInterval(timer);
@@ -219,11 +220,11 @@ export default {
             }
           });
         }, 1000);
-      },3000);
+      }, 3000);
 
     },
 
-    payClick(){
+    payClick() {
       // this.show = !this.show;
       this.show = true;
     },
@@ -231,13 +232,13 @@ export default {
     /*
     * 接口请求
     * */
-    getForum(){
+    getForum() {
       this.appFetch({
-        url:'forum',
-        method:'get',
-        data:{}
-      }).then(res=>{
-        if (res.errors){
+        url: 'forum',
+        method: 'get',
+        data: {}
+      }).then(res => {
+        if (res.errors) {
           this.$toast.fail(res.errors[0].code);
         } else {
           this.sitePrice = res.readdata._data.set_site.site_price;
@@ -253,62 +254,62 @@ export default {
               this.siteExpire = '有效期自加入起' + day + '天';
               break;
           }
-          if (res.readdata._data.paycenter.wxpay_close == true){
-            this.payList.unshift( {
-              name:'微信支付',
-              icon:'icon-wxpay'
+          if (res.readdata._data.paycenter.wxpay_close == true) {
+            this.payList.unshift({
+              name: '微信支付',
+              icon: 'icon-wxpay'
             })
           }
         }
-      }).catch(err=>{
+      }).catch(err => {
       })
     },
-    getOrderSn(){
+    getOrderSn() {
       return this.appFetch({
-        url:'orderList',
-        method:'post',
-        data:{
-          "type":1
+        url: 'orderList',
+        method: 'post',
+        data: {
+          "type": 1
         }
-      }).then(res=>{
-        if (res.errors){
+      }).then(res => {
+        if (res.errors) {
           this.$toast.fail(res.errors[0].code);
         } else {
           this.orderSn = res.readdata._data.order_sn;
         }
-      }).catch(err=>{
+      }).catch(err => {
       })
     },
-    orderPay(type,value){
+    orderPay(type, value) {
       return this.appFetch({
-        url:'orderPay',
-        method:'post',
-        splice:'/' + this.orderSn,
-        data:{
-          "payment_type":type,
-          'pay_password':value
+        url: 'orderPay',
+        method: 'post',
+        splice: '/' + this.orderSn,
+        data: {
+          "payment_type": type,
+          'pay_password': value
         }
-      }).then(res=>{
-        if (res.errors){
+      }).then(res => {
+        if (res.errors) {
           this.$toast.fail(res.errors[0].code);
           this.value = '';
         } else {
-          this.payLoading= true;
+          this.payLoading = true;
           return res;
         }
-      }).catch(err=>{
+      }).catch(err => {
       })
     },
-    getUsersInfo(){
+    getUsersInfo() {
       this.appFetch({
-        url:'users',
-        method:'get',
-        splice:'/' + webDb.getLItem('tokenId'),
-        data:{
-          include:['groups']
+        url: 'users',
+        method: 'get',
+        splice: '/' + webDb.getLItem('tokenId'),
+        data: {
+          include: ['groups']
         }
-      }).then(res=>{
-        if (res.errors){
+      }).then(res => {
+        if (res.errors) {
           this.$toast.fail(res.errors[0].code);
           this.value = '';
         } else {
@@ -323,54 +324,54 @@ export default {
             // clearInterval(time);
           }
         }
-      }).catch(err=>{
+      }).catch(err => {
       })
     },
-    getUsers(id){
+    getUsers(id) {
       return this.appFetch({
-        url:'users',
-        method:'get',
-        splice:'/' + id,
-        headers:{'Authorization': 'Bearer ' + webDb.getLItem('Authorization')},
-        data:{
-          include:['groups']
+        url: 'users',
+        method: 'get',
+        splice: '/' + id,
+        headers: { 'Authorization': 'Bearer ' + webDb.getLItem('Authorization') },
+        data: {
+          include: ['groups']
         }
-      }).then(res=>{
-        if (res.errors){
+      }).then(res => {
+        if (res.errors) {
           this.$toast.fail(res.errors[0].code);
         } else {
           return res;
         }
-      }).catch(err=>{
+      }).catch(err => {
       })
     },
-    getAuthority(id){
+    getAuthority(id) {
       return this.appFetch({
-        url:"authority",
-        method:'get',
-        splice:'/' + id,
-        data:{
-          include:['permission']
+        url: "authority",
+        method: 'get',
+        splice: '/' + id,
+        data: {
+          include: ['permission']
         }
-      }).then(res=>{
-        if (res.errors){
+      }).then(res => {
+        if (res.errors) {
           this.$toast.fail(res.errors[0].code);
         } else {
           return res
         }
-      }).catch(err=>{
+      }).catch(err => {
       })
     },
-    getGroups(){
+    getGroups() {
       this.appFetch({
-        url:'groups',
-        method:'get',
-        data:{
-          include:['permission'],
-          'filter[isDefault]':1
+        url: 'groups',
+        method: 'get',
+        data: {
+          include: ['permission'],
+          'filter[isDefault]': 1
         }
-      }).then(res=>{
-        if(res.errors){
+      }).then(res => {
+        if (res.errors) {
           this.$toast.fail(res.errors[0].code);
         } else {
           this.groupId = res.readdata[0]._data.id;
@@ -378,16 +379,16 @@ export default {
         }
       })
     },
-    getGroupsList(){
+    getGroupsList() {
       this.appFetch({
         url: 'groups',
         method: 'get',
-        splice:'/'+this.groupId,
+        splice: '/' + this.groupId,
         data: {
           include: ['permission'],
         }
       }).then((res) => {
-        if(res.errors){
+        if (res.errors) {
           this.$toast.fail(res.errors[0].code);
         } else {
           this.limitList = res.readdata;
@@ -403,10 +404,10 @@ export default {
     }
 
   },
-  created(){
+  created() {
     this.getForum();
     this.getGroups();
-    this.getUsers(webDb.getLItem('tokenId')).then(res=>{
+    this.getUsers(webDb.getLItem('tokenId')).then(res => {
       this.getAuthority(res.readdata.groups[0]._data.id);
       this.walletBalance = res.readdata._data.walletBalance;
       this.walletStatus = res.readdata._data.canWalletPay;
--- resources/frame/src/template/default/view/m_site/home/circleInviteView.vue
+++ resources/frame/src/template/default/view/m_site/home/circleInviteView.vue
@@ -1,71 +1,150 @@
 <!--移动端付费站点-菜单栏内的邀请模板-->
 
 <template>
-    <div class="circleCon" v-if="siteInfo">
-      <!-- <Header :searchIconShow="false" :perDetShow="true" :logoShow="true" :menuIconShow="false" :navShow="false" :invitePerDet="true" :headFixed="false"></Header> -->
-      <Header :searchIconShow="false" :perDetShow="true" :logoShow="true" :menuIconShow="false" :navShow="false" :invitePerDet="true"></Header>
-	    <div class="gap"></div>
-      <div class="circleInfo padB0 lastBorNone">
-      	<h1 class="cirInfoTit">站点简介</h1>
-      	<p class="cirInfoWord">{{siteInfo._data.set_site.site_introduction}}</p>
-      </div>
-      <div class="gap"></div>
-      <div class="powerListBox" v-if="limitList">
-      	<div class="powerTit">作为{{limitList._data.name}}，您将获得以下权限</div>
-      	<div class="powerList">
-      		<div class="powerClassify">权限列表</div>
-      	  <div class="" v-for="(limit,index) in limitList.permission" :key="index">
-      	    <p class="powerChi" v-if="limit._data.permission && limit._data.permission == 'viewThreads'">查看主题列表</p>
-      	    <p class="powerChi" v-if="limit._data.permission && limit._data.permission == 'thread.viewPosts'">查看主题</p>
-      	    <p class="powerChi" v-if="limit._data.permission && limit._data.permission == 'createThread'">发表主题</p>
-      	    <p class="powerChi" v-if="limit._data.permission && limit._data.permission == 'thread.reply'">回复主题</p>
-      	    <p class="powerChi" v-if="limit._data.permission && limit._data.permission == 'attachment.create.0'">上传附件</p>
-      	    <p class="powerChi" v-if="limit._data.permission && limit._data.permission == 'attachment.create.1'">上传图片</p>
-      	    <p class="powerChi" v-if="limit._data.permission && limit._data.permission == 'attachment.view.0'">查看附件</p>
-      	    <p class="powerChi" v-if="limit._data.permission && limit._data.permission == 'attachment.view.1'">查看图片</p>
-      	    <p class="powerChi" v-if="limit._data.permission && limit._data.permission == 'viewUserList'">站点会员列表</p>
-      	    <p class="powerChi" v-if="limit._data.permission && limit._data.permission == 'attachment.delete'">删除附件</p>
-      	    <p class="powerChi" v-if="limit._data.permission && limit._data.permission == 'cash.create'">申请提现</p>
-      	    <p class="powerChi" v-if="limit._data.permission && limit._data.permission == 'order.create'">创建订单</p>
-      	    <p class="powerChi" v-if="limit._data.permission && limit._data.permission == 'thread.hide'">删除主题</p>
-      	    <p class="powerChi" v-if="limit._data.permission && limit._data.permission == 'thread.hidePosts'">删除回复</p>
-      	    <p class="powerChi" v-if="limit._data.permission && limit._data.permission == 'thread.favorite'">帖子收藏</p>
-      	    <p class="powerChi" v-if="limit._data.permission && limit._data.permission == 'thread.likePosts'">帖子点赞</p>
-      	    <p class="powerChi" v-if="limit._data.permission && limit._data.permission == 'user.view'">查看某个用户信息权限</p>
-      	    <p class="powerChi" v-if="limit._data.permission && limit._data.permission == 'viewSiteInfo'">站点信息</p>
-      	    <p class="powerChi" v-if="limit._data.permission && limit._data.permission == 'user.edit'">编辑用户状态</p>
-      	    <p class="powerChi" v-if="limit._data.permission && limit._data.permission == 'group.edit'">编辑用户组</p>
-      	    <p class="powerChi" v-if="limit._data.permission && limit._data.permission == 'createInvite'">管理-邀请加入</p>
-      	    <p class="powerChi" v-if="limit._data.permission && limit._data.permission == 'thread.batchEdit'">批量管理主题</p>
-      	    <p class="powerChi" v-if="limit._data.permission && limit._data.permission == 'thread.editPosts'">编辑</p>
-      	    <p class="powerChi" v-if="limit._data.permission && limit._data.permission == 'thread.essence'">加精</p>
-      	    <p class="powerChi" v-if="limit._data.permission && limit._data.permission == 'thread.sticky'">置顶</p>
-      	  </div>
-      	</div>
-      </div>
-      <div class="gap"></div>
-      <div class="loginOpera">
-        <a href="javascript:;" @click="loginJump" class="mustLogin">已注册，登录</a>
-        <a href="javascript:;" @click="registerJump" class="regiJoin" v-if="allowRegister">接受邀请，注册</a>
-        <p class="payMoney">￥{{siteInfo._data.set_site.site_price}} / 永久有效</p>
+  <div class="circleCon" v-if="siteInfo">
+    <!-- <Header :searchIconShow="false" :perDetShow="true" :logoShow="true" :menuIconShow="false" :navShow="false" :invitePerDet="true" :headFixed="false"></Header> -->
+    <Header
+      :searchIconShow="false"
+      :perDetShow="true"
+      :logoShow="true"
+      :menuIconShow="false"
+      :navShow="false"
+      :invitePerDet="true"
+    ></Header>
+    <div class="gap"></div>
+    <div class="circleInfo padB0 lastBorNone">
+      <h1 class="cirInfoTit">站点简介</h1>
+      <p class="cirInfoWord">{{siteInfo._data.set_site.site_introduction}}</p>
+    </div>
+    <div class="gap"></div>
+    <div class="powerListBox" v-if="limitList">
+      <div class="powerTit">作为{{limitList._data.name}}，您将获得以下权限</div>
+      <div class="powerList">
+        <div class="powerClassify">权限列表</div>
+        <div class v-for="(limit,index) in limitList.permission" :key="index">
+          <p
+            class="powerChi"
+            v-if="limit._data.permission && limit._data.permission == 'viewThreads'"
+          >查看主题列表</p>
+          <p
+            class="powerChi"
+            v-if="limit._data.permission && limit._data.permission == 'thread.viewPosts'"
+          >查看主题</p>
+          <p
+            class="powerChi"
+            v-if="limit._data.permission && limit._data.permission == 'createThread'"
+          >发表主题</p>
+          <p
+            class="powerChi"
+            v-if="limit._data.permission && limit._data.permission == 'thread.reply'"
+          >回复主题</p>
+          <p
+            class="powerChi"
+            v-if="limit._data.permission && limit._data.permission == 'attachment.create.0'"
+          >上传附件</p>
+          <p
+            class="powerChi"
+            v-if="limit._data.permission && limit._data.permission == 'attachment.create.1'"
+          >上传图片</p>
+          <p
+            class="powerChi"
+            v-if="limit._data.permission && limit._data.permission == 'attachment.view.0'"
+          >查看附件</p>
+          <p
+            class="powerChi"
+            v-if="limit._data.permission && limit._data.permission == 'attachment.view.1'"
+          >查看图片</p>
+          <p
+            class="powerChi"
+            v-if="limit._data.permission && limit._data.permission == 'viewUserList'"
+          >站点会员列表</p>
+          <p
+            class="powerChi"
+            v-if="limit._data.permission && limit._data.permission == 'attachment.delete'"
+          >删除附件</p>
+          <p
+            class="powerChi"
+            v-if="limit._data.permission && limit._data.permission == 'cash.create'"
+          >申请提现</p>
+          <p
+            class="powerChi"
+            v-if="limit._data.permission && limit._data.permission == 'order.create'"
+          >创建订单</p>
+          <p
+            class="powerChi"
+            v-if="limit._data.permission && limit._data.permission == 'thread.hide'"
+          >删除主题</p>
+          <p
+            class="powerChi"
+            v-if="limit._data.permission && limit._data.permission == 'thread.hidePosts'"
+          >删除回复</p>
+          <p
+            class="powerChi"
+            v-if="limit._data.permission && limit._data.permission == 'thread.favorite'"
+          >帖子收藏</p>
+          <p
+            class="powerChi"
+            v-if="limit._data.permission && limit._data.permission == 'thread.likePosts'"
+          >帖子点赞</p>
+          <p
+            class="powerChi"
+            v-if="limit._data.permission && limit._data.permission == 'user.view'"
+          >查看某个用户信息权限</p>
+          <p
+            class="powerChi"
+            v-if="limit._data.permission && limit._data.permission == 'viewSiteInfo'"
+          >站点信息</p>
+          <p
+            class="powerChi"
+            v-if="limit._data.permission && limit._data.permission == 'user.edit'"
+          >编辑用户状态</p>
+          <p
+            class="powerChi"
+            v-if="limit._data.permission && limit._data.permission == 'group.edit'"
+          >编辑用户组</p>
+          <p
+            class="powerChi"
+            v-if="limit._data.permission && limit._data.permission == 'createInvite'"
+          >管理-邀请加入</p>
+          <p
+            class="powerChi"
+            v-if="limit._data.permission && limit._data.permission == 'thread.batchEdit'"
+          >批量管理主题</p>
+          <p
+            class="powerChi"
+            v-if="limit._data.permission && limit._data.permission == 'thread.editPosts'"
+          >编辑</p>
+          <p
+            class="powerChi"
+            v-if="limit._data.permission && limit._data.permission == 'thread.essence'"
+          >加精</p>
+          <p
+            class="powerChi"
+            v-if="limit._data.permission && limit._data.permission == 'thread.sticky'"
+          >置顶</p>
+        </div>
       </div>
     </div>
+    <div class="gap"></div>
+    <div class="loginOpera">
+      <a href="javascript:;" @click="loginJump" class="mustLogin">已注册，登录</a>
+      <a href="javascript:;" @click="registerJump" class="regiJoin" v-if="allowRegister">接受邀请，注册</a>
+      <p class="payMoney">￥{{siteInfo._data.set_site.site_price}} / 永久有效</p>
+    </div>
+  </div>
 </template>
 
 <script>
-import mSiteCircleInviteCon from '../../../controllers/m_site/circle/circleInviteCon';
-import mSiteHeader from '../../../controllers/m_site/common/headerCon';
-import Header from '../../m_site/common/headerView';
+import mSiteCircleInviteCon from "../../../controllers/m_site/circle/circleInviteCon";
+import mSiteHeader from "../../../controllers/m_site/common/headerCon";
+import Header from "../../m_site/common/headerView";
 // import '../../../scss/m_site/mobileIndex.scss';
 export default {
-    name: "circleInviteView",
-    components:{
-    	Header
-    },
-    ...mSiteHeader,
-    ...mSiteCircleInviteCon
-}
-
-
-
+  name: "circleInviteView",
+  components: {
+    Header
+  },
+  ...mSiteHeader,
+  ...mSiteCircleInviteCon
+};
 </script>
