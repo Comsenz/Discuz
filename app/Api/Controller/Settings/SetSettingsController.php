cb4b3160ee9b8d42e1499e0407c4637f38d9fbfb
xinghailong <122363686@qq.com>
2020-03-19 14:22:47 +0800

    [v0.3.200318][bug] 扩展名统一改为小写

--- app/Api/Controller/Settings/SetSettingsController.php
+++ app/Api/Controller/Settings/SetSettingsController.php
@@ -94,7 +94,7 @@ class SetSettingsController implements RequestHandlerInterface
 
         // 扩展名统一改为小写
         $settings->transform(function ($item, $key) {
-            $extArr = ['default_support_img_ext','default_support_file_ext','qcloud_vod_ext'];
+            $extArr = ['default_support_img_ext','default_support_file_ext','qcloud_qcloud_vod_ext'];
             if (in_array($key, $extArr)) {
                 $item['value'] = strtolower($item['value']);
             }
