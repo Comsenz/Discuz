f09b12714a2d1891e52e98df7078ebe840f4fb0d
gyn <920791506@qq.com>
2020-03-30 18:50:42 +0800

    微信分享

--- resources/frame/src/template/default/viewConfig/tplConfig.js
+++ resources/frame/src/template/default/viewConfig/tplConfig.js
@@ -1067,7 +1067,7 @@ export default {
 };
 
 export function wxShare(shareData, toName) {
-  alert(shareData)
+  // alert(shareData)
   console.log(shareData)
   let url = window.location.href.split("#")[0];
   appFetch({
@@ -1083,7 +1083,7 @@ export function wxShare(shareData, toName) {
     let timestamp = res.readdata._data.timestamp;
     let jsApiList = res.readdata._data.jsApiList;
     wx.config({
-      debug: false,          // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+      debug: true,          // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
       appId: appId,         // 必填，公众号的唯一标识
       timestamp: timestamp, // 必填，生成签名的时间戳
       nonceStr: nonceStr,   // 必填，生成签名的随机串
