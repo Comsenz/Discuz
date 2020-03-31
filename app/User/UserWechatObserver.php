c5366545c0928ae97ee054a9d92b6d94396c1266
Harvey <619596123@qq.com>
2020-03-31 15:11:21 +0800

    feat 完成微信头像本地/cos保存

--- app/User/UserWechatObserver.php
+++ app/User/UserWechatObserver.php
@@ -2,51 +2,74 @@
 
 namespace App\User;
 
+use App\Exceptions\TranslatorException;
 use App\Models\UserWechat;
+use Carbon\Carbon;
+use Discuz\Foundation\Application;
+use GuzzleHttp\Client;
+use Illuminate\Contracts\Container\BindingResolutionException;
+use Illuminate\Contracts\Filesystem\Filesystem;
 
 class UserWechatObserver
 {
-    /**
-     * 处理 User「created」事件
-     *
-     * @param UserWechat $userWechat
-     * @return void
-     */
-    public function created(UserWechat $userWechat)
+    protected $file;
+
+    protected $app;
+
+    public function __construct(Filesystem $file, Application $app)
     {
-        dd('created');
+        $this->file = $file;
+        $this->app = $app;
     }
 
     /**
-     * 处理 User「updated」事件
+     * 同步微信头像
      *
      * @param UserWechat $userWechat
-     * @return void
+     * @throws TranslatorException
+     */
+    public function avatarSync($userWechat)
+    {
+        $user = $userWechat->user;
+        if (empty($user)) {
+            return;
+        }
+
+        // 判断用户是否有设置头像
+        if (!empty($user->avatar_at)) {
+            return;
+        }
+
+        // 微信存储本地头像 用到的 HttpClient()
+        $wechatImg = $userWechat->headimgurl;
+        $avatarPath = $userWechat->user_id . '.png';
+
+        $httpClient = new Client();
+
+        $response = $httpClient->request('get', $wechatImg);
+        if ($response->getStatusCode() != 200) {
+            throw new TranslatorException('user_avatar_update_sync_fail');
+        }
+
+        // 获取图片二进制
+        $img = $response->getBody()->getContents();
+
+        $user->changeAvatar($avatarPath);
+
+        $this->file->put($avatarPath, $img);
+
+        $user->avatar_at = Carbon::now()->toDateTimeString();
+        $user->save();
+    }
+
+    /**
+     * 更新User表头像
+     *
+     * @param UserWechat $userWechat
+     * @throws TranslatorException
      */
     public function updated(UserWechat $userWechat)
     {
-        dd('updated');
-    }
-
-    /**
-     * 监听数据即将保存的事件。
-     *
-     * @param  UserWechat $userWechat
-     * @return void
-     */
-    public function saving(UserWechat $userWechat)
-    {
-        dd('saving');
-    }
-
-    /**
-     * 监听数据保存后的事件。
-     *
-     * @param  UserWechat $userWechat
-     * @return void
-     */
-    public function saved(UserWechat $userWechat)
-    {
-        dd('saved');
+        $this->avatarSync($userWechat);
     }
 }
