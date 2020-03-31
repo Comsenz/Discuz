51c2e1ad70fc8fe908174f9f6b3603cf20515646
ljb <1125545287@qq.com>
2020-03-31 16:37:02 +0800

    pc登录api测试

--- app/Commands/Users/WebUserEvent.php
+++ app/Commands/Users/WebUserEvent.php
@@ -10,6 +10,7 @@ use App\Models\SessionToken;
 use App\Models\UserWallet;
 use App\Models\UserWechat;
 use Discuz\Http\DiscuzResponseFactory;
+use EasyWeChat\OfficialAccount\Application;
 use EasyWeChat\Factory;
 use EasyWeChat\Kernel\Messages\Text;
 
@@ -31,7 +32,8 @@ class WebUserEvent
 
     public function handle()
     {
-        $app =Factory::officialAccount($this->wx_config);
+//        $app =Factory::officialAccount($this->wx_config);
+        $app = new Application($this->wx_config);
         $app->server->push(function ($message) {
             if ($message->MsgType == 'event') {
                 switch ($message->Event) {
