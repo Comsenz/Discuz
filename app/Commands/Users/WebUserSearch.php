0ca8e4e52763fee9153672a5252307723f8356b3
ljb <1125545287@qq.com>
2020-04-01 11:29:36 +0800

    pc登录api测试

--- app/Commands/Users/WebUserSearch.php
+++ app/Commands/Users/WebUserSearch.php
@@ -35,7 +35,7 @@ class WebUserSearch
     {
         $this->bus = $bus;
         $this->users = $users;
-        $session = SessionToken::where('token',$this->scene_str)->first();
+        $session = SessionToken::get($this->scene_str);
         $user_id = Arr::get($session, 'user_id');
         $user = User::where('id', $user_id)->first();
         if (isset($user->id) && $user->id != null) {
