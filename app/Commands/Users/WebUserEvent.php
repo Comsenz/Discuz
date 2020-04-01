35ab7100b03db7776671051098702341ca587762
ljb <1125545287@qq.com>
2020-04-01 19:27:16 +0800

    pc用户登录，处理登录逻辑

--- app/Commands/Users/WebUserEvent.php
+++ app/Commands/Users/WebUserEvent.php
@@ -6,13 +6,13 @@
  */
 
 namespace App\Commands\Users;
+
 use App\Models\SessionToken;
 use App\Models\UserWechat;
 use EasyWeChat\Kernel\Messages\Text;
 
 class WebUserEvent
 {
-
     protected $app;
 
     public function __construct($app)
@@ -20,35 +20,32 @@ class WebUserEvent
         $this->app = $app;
     }
 
-
     public function handle()
     {
         $this->app->server->push(function ($message) {
             if (isset($message['MsgType']) && $message['MsgType'] == 'event') {
                 switch ($message->Event) {
                     case 'subscribe':
-                    case "SCAN":
+                    case 'SCAN':
                         return $this->event($message);
                         break;
                 }
             }
         });
-
     }
+
     protected function event($message)
     {
-
         $openid = $message->FromUserName;
         $EventKey = $message->EventKey;
-        $wechatuser = UserWechat::where('mp_openid',$openid)->first();
-        if($wechatuser){
+        $wechatuser = UserWechat::where('mp_openid', $openid)->first();
+        if ($wechatuser) {
             //老用户  跟新扫描二维码用户
-            SessionToken::where('token',$EventKey)->update([
+            SessionToken::where('token', $EventKey)->update([
                     'user_id'=>$wechatuser['user_id'],
                 ]);
             $text = trans('login.WebUser_login_success');
-            return new Text($text);
-        }else{
+        } else {
             //新用户,跳转绑定页面
             $user = $this->app->user->get($openid);
             $user_wechats= new UserWechat();
@@ -60,14 +57,14 @@ class WebUserEvent
             $user_wechats->country = $user->country;
             $user_wechats->headimgurl = $user->headimgurl;
 
-            SessionToken::where('token',$EventKey)->update([
+            SessionToken::where('token', $EventKey)->update([
                 'scope'=>'wechat',
                 'payload'=>['openid'=>$openid]
             ]);
-            if($user_wechats->save()){
+            if ($user_wechats->save()) {
                 $text = trans('login.WebNewUser_login_success');
-                return new Text($text);
             }
         }
+        return new Text($text);
     }
 }
