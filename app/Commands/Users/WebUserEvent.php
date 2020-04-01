33e6ba04423fc214da9c7deb9d0d9172ce7043de
ljb <1125545287@qq.com>
2020-04-01 19:45:40 +0800

    pc用户登录，处理登录逻辑

--- app/Commands/Users/WebUserEvent.php
+++ app/Commands/Users/WebUserEvent.php
@@ -23,7 +23,6 @@ class WebUserEvent
     public function handle()
     {
         $this->app->server->push(function ($message) {
-            return new Text('$message->MsgType');
             if (isset($message->MsgType) && $message->MsgType == 'event') {
                 switch ($message->Event) {
                     case 'subscribe':
