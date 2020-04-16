c04a2e5a02e4743f8b795d3d8549f0e59e52b1e0
Harvey <619596123@qq.com>
2020-04-16 18:40:25 +0800

    chore: 解决冲突v2

--- app/Listeners/Post/PostListener.php
+++ app/Listeners/Post/PostListener.php
@@ -90,37 +90,6 @@ class PostListener
     }
 
     /**
-     * 判断用户是否发表内容时是否@其他人
-     *
-     * @param Created $event
-     */
-    public function RelatedPost(Created $event)
-    {
-        // 判断帖子合法 再发送通知
-        if ($event->post->is_approved == Post::APPROVED) {
-            $post = $event->post;
-            $post_content = $post->content;
-            $actor = $event->actor;
-
-            // 过滤多空格，转化HTML代码
-            $post_content = preg_replace(['/(\s+)/', '/@/', '/</', '/>/'], [' ', ' @', '&lt;', '&gt;'], $post_content);
-
-            // 用户正则 格式：@名字[空格]
-            $user_pattern = "/@([^\r\n]*?)[:|：|，|,|#|\s]/i";
-
-            // 提取用户
-            preg_match_all($user_pattern, $post_content, $userArr);
-
-            if (!empty($userArr[1])) {
-                $relatedIds = User::whereIn('username', $userArr[1])->where('id', '!=', $actor->id)->get();
-                foreach ($relatedIds as $relatedId) {
-                    $relatedId->notify(new Related($post));
-                }
-            }
-        }
-    }
-
-    /**
      * 绑定附件 & 刷新被回复数
      *
      * @param Saved $event
