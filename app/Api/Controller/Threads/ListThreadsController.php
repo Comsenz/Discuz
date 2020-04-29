b845a7b7042e7a72f32caf55784d73bd1b9fd159
ldd <dde-mail@qq.com>
2020-04-29 18:24:24 +0800

    [fixed] thread list filter by type

--- app/Api/Controller/Threads/ListThreadsController.php
+++ app/Api/Controller/Threads/ListThreadsController.php
@@ -265,7 +265,7 @@ class ListThreadsController extends AbstractListController
         }
 
         // 类型：0普通 1长文 2视频 3图片
-        if (($type = Arr::get($filter, 'type')) !== '') {
+        if (($type = Arr::get($filter, 'type', '')) !== '') {
             $query->where('threads.type', (int) $type);
         }
 
