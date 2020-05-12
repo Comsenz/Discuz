bb62266b030b54bdf771d49a734f5f4dfb0bfed3
ldd <dde-mail@qq.com>
2020-05-12 18:24:43 +0800

    fix: 管理员邀请码列表

--- app/Api/Controller/Invite/ListInviteController.php
+++ app/Api/Controller/Invite/ListInviteController.php
@@ -11,6 +11,9 @@ use App\Api\Serializer\InviteSerializer;
 use App\Repositories\InviteRepository;
 use Discuz\Api\Controller\AbstractListController;
 use Discuz\Auth\AssertPermissionTrait;
+use Illuminate\Contracts\Routing\UrlGenerator;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Arr;
 use Psr\Http\Message\ServerRequestInterface;
 use Tobscure\JsonApi\Document;
 
@@ -18,25 +21,72 @@ class ListInviteController extends AbstractListController
 {
     use AssertPermissionTrait;
 
+    /**
+     * {@inheritdoc}
+     */
     public $serializer = InviteSerializer::class;
 
-    protected $InviteRepository;
+    /**
+     * @var InviteRepository
+     */
+    protected $inviteRepository;
 
-    public function __construct(InviteRepository $InviteRepository)
+    /**
+     * @var UrlGenerator
+     */
+    protected $url;
+
+    /**
+     * @param InviteRepository $inviteRepository
+     * @param UrlGenerator $url
+     */
+    public function __construct(InviteRepository $inviteRepository, UrlGenerator $url)
     {
-        $this->InviteRepository = $InviteRepository;
+        $this->inviteRepository = $inviteRepository;
+        $this->url = $url;
     }
 
+    /**
+     * {@inheritdoc}
+     * @throws \Discuz\Auth\Exception\PermissionDeniedException
+     * @throws \Tobscure\JsonApi\Exception\InvalidParameterException
+     */
     protected function data(ServerRequestInterface $request, Document $document)
     {
         $actor = $request->getAttribute('actor');
 
         $this->assertAdmin($actor);
 
-        $data = [];
-        $data['limit'] = $this->extractLimit($request);
-        $data['offset'] = $this->extractOffset($request);
+        $filter = $this->extractFilter($request);
+        $limit = $this->extractLimit($request);
+        $offset = $this->extractOffset($request);
 
-        return $this->InviteRepository->getAdminCodeList($actor, $data);
+        $status = Arr::get($filter, 'status', '');
+
+        $query = $this->inviteRepository->query()
+                    ->where('user_id', $actor->id)
+                    ->where('type', 2)
+                    ->when($status !== '', function (Builder $query) use ($status) {
+                        $query->where('status', $status);
+                    });
+
+        $count = $limit > 0 ? $query->count() : null;
+
+        $list = $query->skip($offset)->take($limit)->orderBy('id', 'desc')->get();
+
+        $document->addPaginationLinks(
+            $this->url->route('invite.list'),
+            $request->getQueryParams(),
+            $offset,
+            $limit,
+            $count
+        );
+
+        $document->setMeta([
+            'total' => $count,
+            'pageCount' => ceil($count / $limit),
+        ]);
+
+        return $list;
     }
 }
