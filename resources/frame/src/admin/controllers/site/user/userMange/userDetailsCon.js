aa8c1626efb86d0fe1ed9cef4b8b58c583cb27c4
gyn <920791506@qq.com>
2020-05-19 10:46:02 +0800

    后台管理系统修改用户到期时间

--- resources/frame/src/admin/controllers/site/user/userMange/userDetailsCon.js
+++ resources/frame/src/admin/controllers/site/user/userMange/userDetailsCon.js
@@ -1,124 +1,124 @@
 /*
-* 用户详情
-* */
+ * 用户详情
+ * */
 
-import Card from '../../../../view/site/common/card/card';
-import CardRow from '../../../../view/site/common/card/cardRow';
-import browserDb from '../../../../../helpers/webDbHelper';
+import Card from "../../../../view/site/common/card/card";
+import CardRow from "../../../../view/site/common/card/cardRow";
+import browserDb from "../../../../../helpers/webDbHelper";
 
 export default {
-  data: function () {
+  data: function() {
     return {
       fileList: [],
       options: [],
       optionsList: [],
-      imageUrl: '',
+      imageUrl: "",
       userRole: [],
       userInfo: {},
-      newPassword: '',
-      wechatNickName: '',
-      sex: '',
+      newPassword: "",
+      wechatNickName: "",
+      sex: "",
       disabled: false,
       disabledReason: false,
-      reasonsForDisable: '',//禁用原用
-      realname: '', //实名认证是否显示
+      reasonsForDisable: "", //禁用原用
+      realname: "", //实名认证是否显示
+      expired_at: "", // 选择过期时间
       optionsStatus: [
         {
           value: 0,
-          label: '正常'
+          label: "正常"
         },
         {
           value: 1,
-          label: '禁用'
+          label: "禁用"
         },
         {
           value: 2,
-          label: '审核'
+          label: "审核"
         }
       ],
-      value: '',
+      value: "",
       query: {},
-      deleBtn: false,
-    }
+      deleBtn: false
+    };
   },
 
   created() {
     this.query = this.$route.query;
     this.getUserDetail();
-    this.getUserList()
+    this.getUserList();
   },
 
   methods: {
     async getUserDetail() {
       try {
-        var userId = browserDb.getLItem('tokenId');
+        var userId = browserDb.getLItem("tokenId");
         const response = await this.appFetch({
-          method: 'get',
-          url: 'users',
+          method: "get",
+          url: "users",
           splice: `/${this.query.id}`,
           data: {
-            include: 'wechat,groups'
+            include: "wechat,groups"
           }
-        })
+        });
         if (response.errors) {
           this.$message.error(response.errors[0].code);
         } else {
           this.userInfo = response.readdata._data;
           this.imageUrl = this.userInfo.avatarUrl;
-          if (this.imageUrl != '' && this.imageUrl != null) {
+          this.expired_at = this.userInfo.expiredAt;
+          if (this.imageUrl != "" && this.imageUrl != null) {
             this.deleBtn = true;
           }
           this.reasonsForDisable = this.userInfo.banReason;
-          this.userRole = response.readdata.groups.map((v) => {
-            return v._data.id
+          this.userRole = response.readdata.groups.map(v => {
+            return v._data.id;
           });
           if (response.readdata.wechat) {
-            this.wechatNickName = response.readdata.wechat._data.nickname
-            this.sex = response.readdata.wechat._data.sex
+            this.wechatNickName = response.readdata.wechat._data.nickname;
+            this.sex = response.readdata.wechat._data.sex;
           }
           if (userId == this.userInfo.id) {
             this.disabled = true;
           }
           if (this.userInfo.status == 1) {
-            this.disabledReason = true
+            this.disabledReason = true;
           }
-          if (this.userInfo.realname == '') {
+          if (this.userInfo.realname == "") {
             this.realname = false;
           } else {
             this.realname = true;
           }
         }
-
-      } catch (err) {
-      }
+      } catch (err) {}
     },
-    handleRemove(file, fileList) {
-    },
+    handleRemove(file, fileList) {},
     deleteImage() {
       if (this.deleBtn == false) {
-        return
+        return;
       }
-      this.imageUrl = '';
+      this.imageUrl = "";
       this.appFetch({
-        url: 'deleteAvatar',
-        method: 'delete',
-        splice: `/${this.query.id}` + '/avatar',
-        data: {
-
-        }
+        url: "deleteAvatar",
+        method: "delete",
+        splice: `/${this.query.id}` + "/avatar",
+        data: {}
       }).then(res => {
         if (res.errors) {
           this.$message.error(res.errors[0].code);
         } else {
           this.deleBtn = false;
-          this.$message.success('删除成功');
+          this.$message.success("删除成功");
         }
-      })
+      });
     },
-    handlePreview(file) {
-    },
+    handlePreview(file) {},
     handleExceed(files, fileList) {
-      this.$message.warning(`当前限制选择 3 个文件，本次选择了 ${files.length} 个文件，共选择了 ${files.length + fileList.length} 个文件`);
+      this.$message.warning(
+        `当前限制选择 3 个文件，本次选择了 ${
+          files.length
+        } 个文件，共选择了 ${files.length + fileList.length} 个文件`
+      );
     },
     beforeRemove(file, fileList) {
       return this.MessageBox.confirm(`确定移除 ${file.name}？`);
@@ -126,47 +126,44 @@ export default {
     handleAvatarSuccess(res, file) {
       // this.imageUrl = URL.createObjectURL(file.raw);
     },
-    handleFile() {
-
-    },
+    handleFile() {},
     beforeAvatarUpload(file) {
-      const isJPG = file.type === 'image/jpeg';
+      const isJPG = file.type === "image/jpeg";
       const isLt10M = file.size / 1024 / 1024 < 10;
 
       if (!isJPG) {
-        this.$message.error('上传头像图片只能是 JPG 格式!');
+        this.$message.error("上传头像图片只能是 JPG 格式!");
       }
       if (!isLt10M) {
-        this.$message.error('上传头像图片大小不能超过 10MB!');
+        this.$message.error("上传头像图片大小不能超过 10MB!");
       }
       if (isJPG && isLt10M == true) {
       }
       return isJPG && isLt10M;
     },
     uploaderLogo(file) {
-      let formData = new FormData()
-      formData.append('avatar', file.file)
+      let formData = new FormData();
+      formData.append("avatar", file.file);
       this.appFetch({
-        url: 'upload',
-        method: 'post',
-        splice: `${this.query.id}` + '/avatar',
+        url: "upload",
+        method: "post",
+        splice: `${this.query.id}` + "/avatar",
         data: formData
       }).then(res => {
         if (res.errors) {
           this.$message.error(res.errors[0].code);
         } else {
-          this.$message.success('上传成功');
+          this.$message.success("上传成功");
           this.imageUrl = res.readdata._data.avatarUrl;
           this.deleBtn = true;
         }
-      })
+      });
     },
 
     submission() {
       var reg = 11 && /^((13|14|15|16|17|18|19)[0-9]{1}\d{8})$/; //手机号正则验证
       var mobile = this.userInfo.originalMobile;
-      if (mobile == '') {
-
+      if (mobile == "") {
       } else if (!reg.test(mobile)) {
         return this.$toast("您输入的手机号码不合法，请重新输入");
       }
@@ -174,61 +171,64 @@ export default {
       // return  this.$toast("您输入的手机号码不合法，请重新输入");
       // }
       this.appFetch({
-        url: 'users',
-        method: 'patch',
+        url: "users",
+        method: "patch",
         splice: `/${this.query.id}`,
         data: {
-          "data": {
-            "attributes": {
-              'newPassword': this.newPassword,
-              'mobile': mobile,
-              'groupId': this.userRole,
-              'status': this.userInfo.status,
-              'refuse_message': this.reasonsForDisable,
+          data: {
+            attributes: {
+              newPassword: this.newPassword,
+              mobile: mobile,
+              groupId: this.userRole,
+              status: this.userInfo.status,
+              refuse_message: this.reasonsForDisable,
+              expired_at: this.expired_at
             }
           }
         }
       }).then(res => {
         if (res.errors) {
           if (res.errors[0].detail) {
-            this.$message.error(res.errors[0].code + '\n' + res.errors[0].detail[0])
+            this.$message.error(
+              res.errors[0].code + "\n" + res.errors[0].detail[0]
+            );
           } else {
             this.$message.error(res.errors[0].code);
           }
         } else {
-          this.$message({ message: '提交成功', type: 'success' });
+          this.$message({ message: "提交成功", type: "success" });
         }
-      })
+      });
     },
 
-    async getUserList() {  //获取用户角色
+    async getUserList() {
+      //获取用户角色
       try {
         const response = await this.appFetch({
-          method: 'get',
-          url: 'groups'
-        })
+          method: "get",
+          url: "groups"
+        });
         const data = response.data;
-        this.options = data.map((v) => {
+        this.options = data.map(v => {
           return {
             value: v.id,
             label: v.attributes.name
-          }
+          };
         });
       } catch (err) {
-        console.error(err, 'getUserList')
+        console.error(err, "getUserList");
       }
     },
     userStatusChange(value) {
       this.disabledReason = value == 1;
       if (value != 1) {
-        this.reasonsForDisable = '';
+        this.reasonsForDisable = "";
       }
     }
-
   },
 
   components: {
     Card,
     CardRow
   }
-}
+};
