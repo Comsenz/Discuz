8acb50240dfc8ccb50004c07f1271295ebad10d1
ldd <dde-mail@qq.com>
2020-05-22 10:42:51 +0800

    feat: 创建分类后给用户组添加分类权限

--- app/Listeners/Category/CategoryListener.php
+++ app/Listeners/Category/CategoryListener.php
@@ -7,16 +7,56 @@
 
 namespace App\Listeners\Category;
 
-use App\Models\Category;
 use App\Events\Category\CategoryRefreshCount;
+use App\Events\Category\Created;
+use App\Models\Category;
+use App\Models\Group;
+use App\Models\GroupPermission;
 use Illuminate\Contracts\Events\Dispatcher;
 
 class CategoryListener
 {
     public function subscribe(Dispatcher $events)
     {
+        // 添加分类时，设置分类下的权限
+        $events->listen(Created::class, [$this, 'whenCategoryCreated']);
+
         // 刷新分类数
         $events->listen(CategoryRefreshCount::class, [$this, 'refreshCount']);
+    }
+
+    /**
+     * @param Created $event
+     */
+    public function whenCategoryCreated(Created $event)
+    {
+        $category = $event->category;
+
+        $groupIds = Group::query()->where('id', '>=', Group::MEMBER_ID)->pluck('id');
+
+        $permissions = $groupIds->reduce(function ($carry, $groupId) use ($category) {
+            return array_merge($carry, [
+                [
+                    'group_id' => $groupId,
+                    'permission' => "category{$category->id}.viewThreads",
+                ],
+                [
+                    'group_id' => $groupId,
+                    'permission' => "category{$category->id}.createThread",
+                ],
+                [
+                    'group_id' => $groupId,
+                    'permission' => "category{$category->id}.replyThread",
+                ],
+            ]);
+        }, [
+            [
+                'group_id' => Group::GUEST_ID,
+                'permission' => "category{$category->id}.viewThreads",
+            ],
+        ]);
+
+        GroupPermission::query()->insert($permissions);
     }
 
     /**
