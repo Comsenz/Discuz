de070938ca1b101ba9b38fb1def2b191c69c8222
xinghailong <122363686@qq.com>
2020-05-28 18:45:33 +0800

    fix(user): 删除用户时删除用户头像

--- app/Observer/UserObserver.php
+++ app/Observer/UserObserver.php
@@ -10,14 +10,19 @@ namespace App\Observer;
 use App\Exceptions\TranslatorException;
 use App\Models\User;
 use Discuz\Contracts\Setting\SettingsRepository;
+use Discuz\Foundation\Application;
+use Illuminate\Contracts\Filesystem\Factory;
 
 class UserObserver
 {
     protected $settings;
 
-    public function __construct(SettingsRepository $settings)
+    protected $app;
+
+    public function __construct(SettingsRepository $settings, Application $app)
     {
         $this->settings = $settings;
+        $this->app = $app;
     }
 
     /**
@@ -40,6 +45,23 @@ class UserObserver
     {
         if ($user->isAdmin()) {
             throw new TranslatorException('user_delete_group_error');
+        }
+    }
+
+    /**
+     * @param User $user
+     */
+    public function deleted(User $user)
+    {
+        //删除用户头像
+        $img = $user->id . '.png';
+
+        // 判断是否是 Cos 地址（如果是 Cos 就删除本地文件，否则删除 Local 文件）
+        if (strpos($user->avatar, '://') === false) {
+            $cosPath = 'public/avatar/' . $img;
+            $this->app->make(Factory::class)->disk('avatar_cos')->delete($cosPath);
+        } else {
+            $this->app->make(Factory::class)->disk('avatar')->delete($img);
         }
     }
 }
