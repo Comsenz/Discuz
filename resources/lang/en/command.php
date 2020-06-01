a87664be70b9462a4ece0092950798069596b991
Harvey <619596123@qq.com>
2020-06-01 18:51:26 +0800

    feat: 新增更新脚本语言包

---
+++ resources/lang/en/command.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'upgrade:category-permission' => 'Initialize category permission for historical data',
+    'upgrade:videoSize' => 'Initialize video size for front display',
+    'upgrade:notice' => 'Update iteration. New notification type data format',
+];
---
+++ resources/lang/zh-CN/command.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'upgrade:category-permission' => '初始化分类权限',
+    'upgrade:videoSize' => '初始化主题视频的宽高',
+    'upgrade:notice' => '更新迭代/新增通知类型数据格式',
+];
