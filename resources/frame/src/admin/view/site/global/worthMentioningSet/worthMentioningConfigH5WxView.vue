0652e3937214bffce13ab39778dd8e6e2fabb165
huangxiaomeng <huangxiaomeng@163.com>
2020-06-04 19:28:08 +0800

    文案排列更改

--- resources/frame/src/admin/view/site/global/worthMentioningSet/worthMentioningConfigH5WxView.vue
+++ resources/frame/src/admin/view/site/global/worthMentioningSet/worthMentioningConfigH5WxView.vue
@@ -19,7 +19,7 @@
     </Card>
 
     <Card header="关闭小程序视频功能：" v-if="type === 'wx_miniprogram'">
-      <CardRow description="开启后，在小程序前台将展示视频内容，并且可进行视频内容的发布。开启前，请务必确保您的小程序已有相应的视频播放类目的权限。">
+      <CardRow description="开启后，在小程序前台将展示视频内容，并且可进行视频内容的发布。开启前，请务必确保您的小程序已有相应的视频播放类目的权限。具体类目权限请">
         <el-switch
           v-model="closeVideo"
           active-color="#336699"
@@ -27,7 +27,7 @@
         >
         </el-switch>
         <template #tail>
-          <a :href="typeCopywriting[type].url" target="_blank" style="margin-left: 15px;">具体类目权限请 点此查看</a>
+          <a :href="typeCopywriting[type].url" target="_blank" style="margin-left: 15px;">点此查看</a>
         </template>
       </CardRow>
     </Card> 
