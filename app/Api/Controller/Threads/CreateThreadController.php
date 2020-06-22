4f25238e618c3955aa313ea631070d935bcdf22b
ldd <dde-mail@qq.com>
2020-06-22 15:20:48 +0800

    fix: 发布主题或回复再加上 images

--- app/Api/Controller/Threads/CreateThreadController.php
+++ app/Api/Controller/Threads/CreateThreadController.php
@@ -29,6 +29,7 @@ class CreateThreadController extends AbstractCreateController
     public $include = [
         'user',
         'firstPost',
+        'firstPost.images',
         'threadVideo',
     ];
 
