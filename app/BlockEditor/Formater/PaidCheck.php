3aa86b1601d2f6d591fdaecbd02c40cc1387ac67
xinghailong <122363686@qq.com>
2020-06-29 18:30:32 +0800

    fix(editor): 付费判断增加guest处理

--- app/BlockEditor/Formater/PaidCheck.php
+++ app/BlockEditor/Formater/PaidCheck.php
@@ -8,16 +8,22 @@
 namespace App\BlockEditor\Formater;
 
 use App\Models\Order;
+use Discuz\Auth\Guest;
 
 class PaidCheck
 {
     public static function isPaid($post_id, array $block_payid)
     {
         $actor = app('request')->getAttribute('actor');
-        return $actor->orders()
-            ->where('status', Order::ORDER_STATUS_PAID)
-            ->where('post_id', $post_id)
-            ->whereIn('block_payid', $block_payid)
-            ->exists();
+
+        if ($actor instanceof Guest) {
+            return false;
+        } else {
+            return $actor->orders()
+                ->where('status', Order::ORDER_STATUS_PAID)
+                ->where('post_id', $post_id)
+                ->whereIn('block_payid', $block_payid)
+                ->exists();
+        }
     }
 }
