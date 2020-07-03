2092699c9074147e617a91bd2c839b9ee1219cb4
xinghailong <122363686@qq.com>
2020-07-03 10:55:32 +0800

    feat(user): 验证增加注册原因

--- resources/lang/zh-CN/validation.php
+++ resources/lang/zh-CN/validation.php
@@ -216,6 +216,7 @@ return [
         'pay_password_confirmation' => '支付密码',
         'groupId' => '用户组',
         'username_prefix' => '网友',
+        'register_reason' => '注册原因',
 
         // thread
         'file_id' => '视频文件',
