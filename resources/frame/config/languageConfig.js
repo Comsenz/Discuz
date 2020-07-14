bc24cd29856a411fedb16ad5b51c58d64242642c
Harvey <619596123@qq.com>
2020-07-14 12:10:19 +0800

    feat: 添加完善后台短信设置验证

--- resources/frame/config/languageConfig.js
+++ resources/frame/config/languageConfig.js
@@ -49,6 +49,7 @@ const config = {
   offIAccount_server_config_signature_failed: '公众号服务器配置：签名验证失败',
   qcloud_vod_cover_template_not_found: '腾讯云云点播截图模板不存在',
   tencent_vod_error : '腾讯云云点播配置错误',
+  tencent_qcloud_sms_app_error : '腾讯云短信配置错误',
 };
 
 Vue.prototype.getLang = function (key) {
