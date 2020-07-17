290ccf6b96a22333eaba8a374f55a8d83550b69a
ldd <dde-mail@qq.com>
2020-07-17 15:57:49 +0800

    fix: 修改将邀请设为无效的权限

--- app/Policies/InvitePolicy.php
+++ app/Policies/InvitePolicy.php
@@ -22,40 +22,13 @@ class InvitePolicy extends AbstractPolicy
 
     /**
      * @param User $actor
-     * @param Model $model
-     * @param string $ability
+     * @param Invite $invite
      * @return bool|null
      */
-    public function canPermission(User $actor, Model $model, $ability)
+    public function delete(User $actor, Invite $invite)
     {
-        if ($actor->hasPermission($this->getAbility($ability))) {
+        if ($actor->hasPermission('createInvite') && ($invite->user_id == $actor->id || $actor->isAdmin())) {
             return true;
-        }
-    }
-
-    /**
-     * @param User $actor
-     * @param Builder $query
-     * @return void
-     */
-    public function findVisibility(User $actor, Builder $query)
-    {
-        // 当前用户是否有权限查看
-        if ($actor->cannot('viewDiscussions')) {
-            $query->whereRaw('FALSE');
-            return;
-        }
-    }
-
-    /**
-     * @param User $actor
-     * @param Builder $query
-     * @return void
-     */
-    public function findEditVisibility(User $actor, Builder $query)
-    {
-        if ($actor->cannot('editInvite')) {
-            $query->where('invites.user_id', $actor->id);
         }
     }
 }
