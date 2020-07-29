0f57f551738f87ed35def043178e64870fe9afab
ldd <dde-mail@qq.com>
2020-07-29 10:49:49 +0800

    fix: 新增验证码验证规则 Captcha

--- app/Validators/ThreadValidator.php
+++ app/Validators/ThreadValidator.php
@@ -18,75 +18,27 @@
 
 namespace App\Validators;
 
-use Discuz\Contracts\Setting\SettingsRepository;
+use App\Rules\Captcha;
 use Discuz\Foundation\AbstractValidator;
-use Discuz\Qcloud\QcloudTrait;
-use Illuminate\Validation\Factory;
 
 class ThreadValidator extends AbstractValidator
 {
-    use QcloudTrait;
-
-    /**
-     * @var SettingsRepository
-     */
-    protected $settings;
-
-    /**
-     * 腾讯云验证码开关。
-     *
-     * @var bool
-     */
-    protected $qCloudCaptchaSwitch = false;
-
-    /**
-     * 获取相关设置
-     *
-     * @param Factory $validator
-     * @param SettingsRepository $settings
-     */
-    public function __construct(Factory $validator, SettingsRepository $settings)
-    {
-        parent::__construct($validator);
-
-        $this->settings = $settings;
-
-        // 获取后台设置的腾讯云验证码开关
-        $this->qCloudCaptchaSwitch = (bool)$settings->get('qcloud_captcha', 'qcloud');
-    }
-
     /**
      * {@inheritdoc}
      */
     protected function getRules()
     {
-        $rules = [
+        return [
             'title' => 'required|max:80',
             'price' => [
                 'sometimes',
                 'regex:/^(0|[1-9]\d{0,5})(\.\d{1,2})?$/',   // decimal 10,2
             ],
-        ];
-
-        // 当腾讯云验证码开启时，且数据中有 captcha 时，检查验证码
-        if ($this->qCloudCaptchaSwitch) {
-            $rules['captcha'] = [
+            'captcha' => [
                 'sometimes',
-                function ($attribute, $value, $fail) {
-                    if (count(array_filter($value)) == 3) {
-                        $result = $this->describeCaptchaResult(...$value);
-
-                        if ($result['CaptchaCode'] != 1) {
-                            $fail(trans('validation.wrong') . "({$result['CaptchaCode']})");
-                        }
-                    } else {
-                        $fail(trans('validation.wrong'));
-                    }
-                },
-            ];
-        }
-
-        return $rules;
+                new Captcha,
+            ],
+        ];
     }
 
     /**
