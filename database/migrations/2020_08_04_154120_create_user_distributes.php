1373b33457d02ee32139e29466a287fe7c107f32
Harvey <619596123@qq.com>
2020-08-05 18:09:14 +0800

    feat: 完成互相关注/创建上下级关系

---
+++ database/migrations/2020_08_04_154120_create_user_distributes.php
@@ -0,0 +1,42 @@
+<?php
+
+use Discuz\Database\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateUserDistributes extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $this->schema()->create('user_distributes', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('pid')->nullable()->comment('父级id');
+            $table->unsignedBigInteger('user_id')->nullable()->comment('用户ID');
+            $table->char('invites_code', 32)->default('')->comment('关联邀请码');
+            $table->unsignedBigInteger('be_scale')->default(0)->comment('受邀时的分成比例');
+            $table->tinyInteger('level')->default(1)->comment('当前用户所处深度');
+
+            $table->dateTime('updated_at')->comment('更新时间');
+            $table->dateTime('created_at')->comment('创建时间');
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
+            $table->index('user_id', 'idx_user');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $this->schema()->dropIfExists('user_distributes');
+    }
+}
