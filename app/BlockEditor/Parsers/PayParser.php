7991b75f10645c605a5147b681f0df128f736bab
zz <583417300@qq.com>
2020-08-18 15:47:04 +0800

    付费块id检查

--- app/BlockEditor/Parsers/PayParser.php
+++ app/BlockEditor/Parsers/PayParser.php
@@ -8,14 +8,16 @@
 namespace App\BlockEditor\Parsers;
 
 use Illuminate\Support\Str;
+use Illuminate\Support\Arr;
 
 class PayParser extends BaseParser
 {
     public static function checkPayID(array $data)
     {
-        if (empty($data['blockPayid']) || strlen($data['blockPayid']) != 36) {
+        $block_pay_id = Arr::get($data, 'blockPayid');
+        if (empty($block_pay_id) || strlen($block_pay_id) != 36) {
            return (string) Str::uuid();
         }
-        return $data['blockPayid'];
+        return $block_pay_id;
     }
 }
