1677819bd1c5517e71974fa79cdb822b379f9ee1
xinghailong <122363686@qq.com>
2020-08-18 15:28:04 +0800

    fix(thread):  title可以为空

---
+++ database/migrations/2020_08_18_152352_change_threads_title_null.php
@@ -0,0 +1,31 @@
+<?php
+
+use Discuz\Database\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class ChangeThreadsTitleNull extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $this->schema()->table('threads', function (Blueprint $table) {
+            $table->string('title')->comment('标题')->nullable()->default(null)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $this->schema()->table('user_wechats', function (Blueprint $table) {
+            $table->string('title')->default('')->comment('标题')->change();
+        });
+    }
+}
