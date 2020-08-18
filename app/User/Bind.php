4abce6e36718c9d4e4825a219e678e89a486fe57
xinghailong <122363686@qq.com>
2020-08-18 17:17:36 +0800

    fix(miniprogram): 小程序已绑定用户可以正常更新userwechat数据并返回供绑定用户无感登陆使

--- app/User/Bind.php
+++ app/User/Bind.php
@@ -125,7 +125,7 @@ class Bind
         })->orWhere('min_openid', $openid)->first();
 
         // 非无感模式，用户、微信已经存在绑定关系，抛出异常
-        if ($this->settings->get('register_type') != 2 && $isMiniProgramLogin) {
+        if ($this->settings->get('register_type') != 2 && !$isMiniProgramLogin) {
             if (!is_null($user->wechat) || ($wechatUser && $wechatUser->user_id)) {
                 throw new Exception('account_has_been_bound');
             }
