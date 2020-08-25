46fa6f95a00637eb4d09c39ebd6e9310e2c8451e
ldd <dde-mail@qq.com>
2020-08-25 16:36:22 +0800

    feat: 位置服务开关

--- app/Api/Serializer/ForumSettingSerializer.php
+++ app/Api/Serializer/ForumSettingSerializer.php
@@ -152,6 +152,7 @@ class ForumSettingSerializer extends AbstractSerializer
             ],
 
             'lbs' => [
+                'lbs' => (bool) $this->settings->get('lbs', 'lbs'),         // 位置服务开关
                 'qq_lbs_key' => $this->settings->get('qq_lbs_key', 'lbs'),  // 腾讯位置服务 key
             ]
         ];
