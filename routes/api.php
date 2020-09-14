85003d9e48384159e58e3f153752de72b1c93557
xinghailong <122363686@qq.com>
2020-09-14 11:06:29 +0800

    feat(user): 推荐用户

--- routes/api.php
+++ routes/api.php
@@ -117,6 +117,7 @@ $route->delete('/users/{id}/deny', 'user.delete.deny', ApiController\Users\Delet
 $route->delete('/users/{id}/avatar', 'user.delete.avatar', ApiController\Users\DeleteAvatarController::class);
 $route->delete('/users/{id}/wechat', 'user.delete.wechat', ApiController\Users\UnbindWechatController::class);
 $route->get('/export/users', 'export.users', ApiController\Users\ExportUserController::class);
+$route->get('/users/recommended', 'user.recommended', ApiController\Users\RecommendedUserController::class);
 
 /*
 |--------------------------------------------------------------------------
