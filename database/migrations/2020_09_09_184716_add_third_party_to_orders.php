ce1fcbd34bb93d7aade36401c3b9d485b2bfeaec
Harvey <619596123@qq.com>
2020-09-18 11:06:48 +0800

    fix: 修复订单表第三者受益人默认null

--- database/migrations/2020_09_09_184716_add_third_party_to_orders.php
+++ database/migrations/2020_09_09_184716_add_third_party_to_orders.php
@@ -13,7 +13,7 @@ class AddThirdPartyToOrders extends Migration
     public function up()
     {
         $this->schema()->table('orders', function (Blueprint $table) {
-            $table->unsignedBigInteger('third_party_id')->after('payee_id')->comment('第三者收益人 id');
+            $table->unsignedBigInteger('third_party_id')->nullable()->after('payee_id')->comment('第三者收益人 id');
             $table->decimal('third_party_amount')->unsigned()->default(0)->after('author_amount')->comment('第三者收益金额');
         });
     }
