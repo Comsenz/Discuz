e274de5fcfe35b735fac9d67af821beb83c4d2e6
Harvey <619596123@qq.com>
2020-09-22 16:21:13 +0800

    build: 返回站长分成比例值，用于前端计算围观分成

--- app/Api/Serializer/ForumSettingSerializer.php
+++ app/Api/Serializer/ForumSettingSerializer.php
@@ -74,6 +74,7 @@ class ForumSettingSerializer extends AbstractSerializer
                 'site_cover' => $this->settings->get('site_cover') ?: '',
                 'site_record_code' => $this->settings->get('site_record_code') ?: '',
                 'site_onlooker_price' => $this->settings->get('site_onlooker_price') ?: 0, // 默认围观值前端根据权限判断
+                'site_master_scale' => $this->settings->get('site_master_scale'), // 站长比例
             ],
 
             // 注册设置
