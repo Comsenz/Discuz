32fa796ceb4b953c66326f8873a34c0209a0aaab
Harvey <619596123@qq.com>
2020-10-10 18:03:00 +0800

    fix: 修复问答信息被删除时判断

--- app/Api/Serializer/ThreadSerializer.php
+++ app/Api/Serializer/ThreadSerializer.php
@@ -120,6 +120,12 @@ class ThreadSerializer extends AbstractSerializer
             return;
         }
 
+        // 判断问答信息是否存在
+        if (empty($model->question)) {
+            $attributes['onlookerState'] = false;
+            return;
+        }
+
         /**
          * 判断是否围观过帖子
          */
@@ -143,7 +149,7 @@ class ThreadSerializer extends AbstractSerializer
          * 判断是否匿名问答
          * (非当前用户不是作者)
          */
-        if ($model->question && $model->question->is_anonymous && $model->user->id != $this->actor->id) {
+        if ($model->question->is_anonymous && $model->user->id != $this->actor->id) {
             $model->user = new Anonymous;
         }
     }
