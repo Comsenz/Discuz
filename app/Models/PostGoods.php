f3278f98e0808b98ddc06461773d0a1325eee27d
Harvey <619596123@qq.com>
2020-10-13 18:51:21 +0800

    fix: 添加商品序列化时间返回

--- app/Models/PostGoods.php
+++ app/Models/PostGoods.php
@@ -53,6 +53,14 @@ class PostGoods extends Model
     public static $key;
 
     /**
+     * {@inheritdoc}
+     */
+    protected $dates = [
+        'created_at',
+        'updated_at',
+    ];
+
+    /**
      * 允许的域名
      *  0淘宝 1天猫 2京东 3拼多多H5 4有赞 5淘宝口令粘贴值 6京东粘贴值H5域名 7有赞粘贴值
      *
