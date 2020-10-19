c035f4971a6ff4f2e305f825d711b5b2dc364d0b
Harvey <619596123@qq.com>
2020-10-19 17:19:01 +0800

    fix: 修复免费的问答免费的围观

--- app/Api/Serializer/QuestionAnswerSerializer.php
+++ app/Api/Serializer/QuestionAnswerSerializer.php
@@ -58,11 +58,6 @@ class QuestionAnswerSerializer extends AbstractSerializer
             'answered_at' => $this->formatDate($model->answered_at),
         ];
 
-        // 前端用作判断是否渲染答案
-        if ($model->price == 0 && $model->onlooker_unit_price == 0) {
-            $attributes['is_onlooker'] = true;
-        }
-
         // 判断是否已围观来展示答案
         if (
             $model->user_id == $actor->id
