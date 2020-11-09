d4e174b009eee0b56e54ec921905f30e0b7fdd34
coralchu <coralchu@tencent.com>
2020-11-09 16:24:44 +0800

    添加各个端是否开启功能

--- app/Api/Serializer/ForumSettingSerializer.php
+++ app/Api/Serializer/ForumSettingSerializer.php
@@ -66,7 +66,8 @@ class ForumSettingSerializer extends AbstractSerializer
                 'site_keywords' => $this->settings->get('site_keywords'),
                 'site_introduction' => $this->settings->get('site_introduction'),
                 'site_mode' => $this->settings->get('site_mode'), // pay public
-                'site_close' => (bool)$this->settings->get('site_close'),
+//                'site_close' => (bool)$this->settings->get('site_close'),
+                'site_manage' => json_decode($this->settings->get('site_manage'), true),
                 'site_favicon' => $favicon ?: app(UrlGenerator::class)->to('/favicon.ico'),
                 'site_logo' => $logo ?: '',
                 'site_header_logo' => $headerLogo ?: '',
