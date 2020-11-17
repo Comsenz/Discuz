d88a0533195841dea321c7a65a8f344b7caf9d27
xinghailong <122363686@qq.com>
2020-11-17 10:48:59 +0800

    feat(video): 在云点播平台删除视频时 同步删除主题

--- app/Commands/Thread/Notify/ThreadVideoNotify.php
+++ app/Commands/Thread/Notify/ThreadVideoNotify.php
@@ -25,9 +25,10 @@ use App\Models\ThreadVideo;
 use App\Repositories\PostRepository;
 use App\Repositories\ThreadRepository;
 use App\Repositories\ThreadVideoRepository;
+use Carbon\Carbon;
+use Discuz\Foundation\EventsDispatchTrait;
 use Discuz\Qcloud\QcloudTrait;
 use Illuminate\Contracts\Events\Dispatcher;
-use Discuz\Foundation\EventsDispatchTrait;
 use Illuminate\Support\Arr;
 
 class ThreadVideoNotify
@@ -66,78 +67,98 @@ class ThreadVideoNotify
         }
 
         todo:
-        //只处理视频处理类型的通知
-        if (Arr::get($this->data, 'EventType') != 'ProcedureStateChanged') {
+        //只处理视频处理类型、视频删除的通知
+        $EventType = Arr::get($this->data, 'EventType');
+        if (!in_array($EventType, ['ProcedureStateChanged', 'FileDeleted'])) {
             return 'pass';
         }
-        $taskDetail  = $this->describeTaskDetail(Arr::get($this->data, 'ProcedureStateChangeEvent.TaskId'));
-        if ($taskDetail && $taskDetail->TaskType == 'Procedure' && $taskDetail->Status == 'FINISH') {
-            if ($taskDetail->ProcedureTask->Status == 'FINISH') {
 
-                /** @var ThreadVideo $threadVideo */
-                $threadVideo = $threadVideos->findOrFailByFileId($taskDetail->ProcedureTask->FileId);
+        if ($EventType == 'ProcedureStateChanged') {
+            $taskDetail  = $this->describeTaskDetail(Arr::get($this->data, 'ProcedureStateChangeEvent.TaskId'));
+            if ($taskDetail && $taskDetail->TaskType == 'Procedure' && $taskDetail->Status == 'FINISH') {
+                if ($taskDetail->ProcedureTask->Status == 'FINISH') {
 
-                foreach ($taskDetail->ProcedureTask->MediaProcessResultSet as $key => $value) {
-                    //普通转码
-                    if ($value->Type == 'Transcode') {
-                        if ($value->TranscodeTask->ErrCode == 0) {
-                            //转码成功
-                            $threadVideo->status = ThreadVideo::VIDEO_STATUS_SUCCESS;
-                            $threadVideo->media_url = $value->TranscodeTask->Output->Url;
-                            $threadVideo->width = $value->TranscodeTask->Output->Width;
-                            $threadVideo->height = $value->TranscodeTask->Output->Height;
-                            $threadVideo->duration = $value->TranscodeTask->Output->Duration;
-                        } else {
-                            //转码失败
-                            $threadVideo->status = ThreadVideo::VIDEO_STATUS_FAIL;
-                            $threadVideo->reason = $value->TranscodeTask->Message;
+                    /** @var ThreadVideo $threadVideo */
+                    $threadVideo = $threadVideos->findOrFailByFileId($taskDetail->ProcedureTask->FileId);
+
+                    foreach ($taskDetail->ProcedureTask->MediaProcessResultSet as $key => $value) {
+                        //普通转码
+                        if ($value->Type == 'Transcode') {
+                            if ($value->TranscodeTask->ErrCode == 0) {
+                                //转码成功
+                                $threadVideo->status = ThreadVideo::VIDEO_STATUS_SUCCESS;
+                                $threadVideo->media_url = $value->TranscodeTask->Output->Url;
+                                $threadVideo->width = $value->TranscodeTask->Output->Width;
+                                $threadVideo->height = $value->TranscodeTask->Output->Height;
+                                $threadVideo->duration = $value->TranscodeTask->Output->Duration;
+                            } else {
+                                //转码失败
+                                $threadVideo->status = ThreadVideo::VIDEO_STATUS_FAIL;
+                                $threadVideo->reason = $value->TranscodeTask->Message;
+                            }
                         }
-                    }
 
-                    //自适应码流转码
-                    if ($value->Type == 'AdaptiveDynamicStreaming') {
-                        if ($value->AdaptiveDynamicStreamingTask->ErrCode == 0) {
-                            //转码成功
-                            $threadVideo->status = ThreadVideo::VIDEO_STATUS_SUCCESS;
-                            $threadVideo->media_url = $value->AdaptiveDynamicStreamingTask->Output->Url;
-                        } else {
-                            //转码失败
-                            $threadVideo->status = ThreadVideo::VIDEO_STATUS_FAIL;
-                            $threadVideo->reason = $value->AdaptiveDynamicStreamingTask->Message;
+                        //自适应码流转码
+                        if ($value->Type == 'AdaptiveDynamicStreaming') {
+                            if ($value->AdaptiveDynamicStreamingTask->ErrCode == 0) {
+                                //转码成功
+                                $threadVideo->status = ThreadVideo::VIDEO_STATUS_SUCCESS;
+                                $threadVideo->media_url = $value->AdaptiveDynamicStreamingTask->Output->Url;
+                            } else {
+                                //转码失败
+                                $threadVideo->status = ThreadVideo::VIDEO_STATUS_FAIL;
+                                $threadVideo->reason = $value->AdaptiveDynamicStreamingTask->Message;
+                            }
                         }
-                    }
 
-                    if ($value->Type == 'CoverBySnapshot') {
-                        if ($value->CoverBySnapshotTask->ErrCode == 0) {
-                            //截取封面图成功
-                            $threadVideo->cover_url = $value->CoverBySnapshotTask->Output->CoverUrl;
+                        if ($value->Type == 'CoverBySnapshot') {
+                            if ($value->CoverBySnapshotTask->ErrCode == 0) {
+                                //截取封面图成功
+                                $threadVideo->cover_url = $value->CoverBySnapshotTask->Output->CoverUrl;
+                            }
                         }
-                    }
-                    //动图任务流
-                    if ($value->Type == 'AnimatedGraphics') {
-                        if ($value->AnimatedGraphicTask->ErrCode == 0) {
-                            //截取封面图成功
-                            $threadVideo->cover_url = $value->AnimatedGraphicTask->Output->Url;
+                        //动图任务流
+                        if ($value->Type == 'AnimatedGraphics') {
+                            if ($value->AnimatedGraphicTask->ErrCode == 0) {
+                                //截取封面图成功
+                                $threadVideo->cover_url = $value->AnimatedGraphicTask->Output->Url;
+                            }
                         }
                     }
                 }
+
+                $threadVideo->save();
+
+                //主题审核状态
+                $post = $posts->query()->where('thread_id', $threadVideo->thread_id)->firstOrFail();
+                $censor->checkText($post->content);
+                if (!$censor->isMod && $threadVideo->status == ThreadVideo::VIDEO_STATUS_SUCCESS) {
+                    $thread = $threads->findOrFail($threadVideo->thread_id);
+                    $thread->is_approved = 1;
+                    $thread->save();
+
+                    //解析并创建话题和关系
+                    BaseFormatter::setActor($thread->user);
+                    $thread->firstPost->setContentAttribute($thread->firstPost->content);
+                    $thread->firstPost->save();
+                    ThreadTopic::setThreadTopic($thread->firstPost);
+                }
             }
-
-            $threadVideo->save();
-
-            //主题审核状态
-            $post = $posts->query()->where('thread_id', $threadVideo->thread_id)->firstOrFail();
-            $censor->checkText($post->content);
-            if (!$censor->isMod && $threadVideo->status == ThreadVideo::VIDEO_STATUS_SUCCESS) {
-                $thread = $threads->findOrFail($threadVideo->thread_id);
-                $thread->is_approved = 1;
-                $thread->save();
-
-                //解析并创建话题和关系
-                BaseFormatter::setActor($thread->user);
-                $thread->firstPost->setContentAttribute($thread->firstPost->content);
-                $thread->firstPost->save();
-                ThreadTopic::setThreadTopic($thread->firstPost);
+        } elseif ($EventType == 'FileDeleted') {
+            // 删除的视频逻辑删除主题
+            $FileIdSet = Arr::get($this->data, 'FileDeleteEvent.FileIdSet');
+            foreach ($FileIdSet as $key => $value) {
+                $detail  = $this->describeMediaInfos([$value], ['']);
+                if (count($detail->NotExistFileIdSet) > 0) {
+                    try {
+                        /** @var ThreadVideo $threadVideo */
+                        $threadVideo = $threadVideos->findOrFailByFileId($value);
+                        $threadVideo->thread->deleted_at = Carbon::now();
+                        $threadVideo->thread->save();
+                    } catch (\Exception $e) {
+                        continue;
+                    }
+                }
             }
         }
 
